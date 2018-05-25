b9e142205401f9d7892360753c235f15e1c17a97
Georgios Manoltzas <g.manoltzas@indice.gr>
2018-05-25 18:20:15 +0300

    add extension to register services in di

---
+++ src/Indice.AspNetCore/Extensions/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Indice.Abstractions;
+using Indice.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Indice.AspNetCore.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddIndiceServices(this IServiceCollection services) {
+            services.AddTransient<IMarkdownProcessor, MarkdigProcessor>();
+            return services;
+        }
+    }
+}
