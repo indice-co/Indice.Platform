58a7e1f94a7ba91a00062053ef9b60a50cd5e40e
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2018-06-08 18:27:55 +0300

    sort by clause tests with datatype

--- test/Indice.Common.Tests/SortByClauseTests.cs
+++ test/Indice.Common.Tests/SortByClauseTests.cs
@@ -8,7 +8,7 @@ namespace Indice.Common.Tests
     public class SortByClauseTests
     {
         [Fact]
-        public void SortByClauseParsingTest()
+        public void SortByClauseListOptionsTest()
         {
             var options = new ListOptions() {
                 Sort = "code-"
@@ -17,6 +17,14 @@ namespace Indice.Common.Tests
             Assert.Single(list);
             Assert.Equal("code", list[0].Path);
             Assert.Equal(options.Sort, list[0].ToString());
+        }
+
+
+        [Theory]
+        [InlineData("(number)total-")]
+        public void SortByClauseParsingTest(string input) {
+            var sortBy = SortByClause.Parse(input);
+            Assert.Equal(input, sortBy.ToString());
         }
     }
 }
