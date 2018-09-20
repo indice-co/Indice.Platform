44aa821b0e22d070c63a45eb2bab8f3be4a7774b
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2018-09-20 16:51:07 +0300

    CSP action filter.

--- src/Indice.AspNetCore.Identity/Filters/SecurityHeadersAttribute.cs
+++ src/Indice.AspNetCore.Identity/Filters/SecurityHeadersAttribute.cs
@@ -8,12 +8,21 @@ using Newtonsoft.Json;
 
 namespace Indice.AspNetCore.Identity.Filters
 {
+    /// <summary>
+    /// Sets the Content Security policy header for the current action.
+    /// </summary>
     public class SecurityHeadersAttribute : ActionFilterAttribute
     {
         private readonly CSP _csp;
 
+        /// <summary>
+        /// Constractor Defaults to allowing self origin plus google for fonts and scripts (google cdn) and wildcard for images.
+        /// </summary>
         public SecurityHeadersAttribute() : this(null) { }
-
+        
+        /// <summary>
+        /// Constractor with custom security policy.
+        /// </summary>
         public SecurityHeadersAttribute(CSP scp) {
             _csp = scp ?? new CSP {
                 DefaultSrc = $"{CSP.Self}",
@@ -23,6 +32,10 @@ namespace Indice.AspNetCore.Identity.Filters
             };
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
         public override void OnResultExecuting(ResultExecutingContext context) {
             var result = context.Result;
 
@@ -74,60 +87,92 @@ namespace Indice.AspNetCore.Identity.Filters
         /// </summary>
         public const string Data = "data:";
 
+        /// <summary>
+        /// Content Security policy regarding all sources.
+        /// </summary>
         public string DefaultSrc {
             get => GetValueOrDefult(nameof(DefaultSrc));
             set => SetValue(nameof(DefaultSrc), value);
         }
 
+        /// <summary>
+        /// Content Security policy regarding script sources.
+        /// </summary>
         public string ScriptSrc {
             get => GetValueOrDefult(nameof(ScriptSrc));
             set => SetValue(nameof(ScriptSrc), value);
         }
 
+        /// <summary>
+        /// Content Security policy regarding cascading stylesheets.
+        /// </summary>
         public string StyleSrc {
             get => GetValueOrDefult(nameof(StyleSrc));
             set => SetValue(nameof(StyleSrc), value);
         }
 
+        /// <summary>
+        /// Content Security policy regarding image sources.
+        /// </summary>
         public string ImgSrc {
             get => GetValueOrDefult(nameof(ImgSrc));
             set => SetValue(nameof(ImgSrc), value);
         }
 
+        /// <summary>
+        /// Content Security policy regarding font sources.
+        /// </summary>
         public string FontSrc {
             get => GetValueOrDefult(nameof(FontSrc));
             set => SetValue(nameof(FontSrc), value);
         }
 
+        /// <summary>
+        /// Endpoint to report back the csp report from server.
+        /// </summary>
         public string ReportUri {
             get => GetValueOrDefult(nameof(ReportUri));
             set => SetValue(nameof(ReportUri), value);
         }
 
+        /// <summary>
+        /// Gets the <see cref="IEnumerator{T}"/> to iterate the underliing values for each policy part.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<string> GetEnumerator() => values.Where(x => x.Value != null).Select(x => $"{GetUrlCasing(x.Key)} {x.Value}").GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        /// <summary>
+        /// The string representation of the CSP header value. Ready to use.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString() => string.Join("; ", this);
-
-        private string GetValueOrDefult(string key) => values.ContainsKey(nameof(key)) ? values[nameof(key)] : null;
+        
+        private string GetValueOrDefult(string key) => values.ContainsKey(key) ? values[key] : null;
 
         private void SetValue(string key, string value) {
-            if (values.ContainsKey(nameof(key))) {
-                values[nameof(key)] = value;
+            if (values.ContainsKey(key)) {
+                values[key] = value;
             } else {
-                values.Add(nameof(key), value);
+                values.Add(key, value);
             }
         }
 
         private string GetUrlCasing(string pascalCasing) => Regex.Replace(pascalCasing, "([A-Z][a-z]+)", "-$1", RegexOptions.Compiled).Trim().ToLowerInvariant();
 
+        /// <summary>
+        /// Helper object that represents a browser CSP report request object.
+        /// </summary>
         public class ReportRequest
         {
             [JsonProperty(PropertyName = "csp-report")]
             public Report CspReport { get; set; }
         }
 
+        /// <summary>
+        /// Helper object that represents a browser CSP report object.
+        /// </summary>
         public class Report
         {
             [JsonProperty(PropertyName = "document-uri")]
