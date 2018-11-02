c3417c64aaa60c99101c94d795c06ca6171c43e5
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2018-11-02 14:18:48 +0200

    changes regarding account models

--- src/Indice.AspNetCore.Identity/Models/RedirectViewModel.cs
+++ src/Indice.AspNetCore.Identity/Models/RedirectViewModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace Indice.AspNetCore.Identity.Models
 {
+    /// <summary>
+    /// Redirect view model.
+    /// </summary>
     public class RedirectViewModel
     {
+        /// <summary>
+        /// Url to redirect to
+        /// </summary>
         public string RedirectUrl { get; set; }
     }
 }
--- src/Indice.AspNetCore.Identity/Models/RegisterViewModel.cs
+++ src/Indice.AspNetCore.Identity/Models/RegisterViewModel.cs
@@ -3,11 +3,30 @@ using System.Linq;
 
 namespace Indice.AspNetCore.Identity.Models
 {
+    /// <summary>
+    /// This is a subclass of the <see cref="RegisterRequest"/> that also contains the external Identity providers that are setup.
+    /// Only used on the MVC Views. 
+    /// </summary>
     public class RegisterViewModel : RegisterRequest
     {
+        /// <summary>
+        /// List of external providers.
+        /// </summary>
         public IEnumerable<ExternalProvider> ExternalProviders { get; set; }
+
+        /// <summary>
+        /// Visible external providers are those given a <see cref="ExternalProvider.DisplayName"/>
+        /// </summary>
         public IEnumerable<ExternalProvider> VisibleExternalProviders => ExternalProviders.Where(x => !string.IsNullOrWhiteSpace(x.DisplayName));
+
+        /// <summary>
+        /// Optional flag that should hide the local user registration form and keep only the <see cref="ExternalProviders"/> options.
+        /// </summary>
         public bool ExternalRegistrationOnly { get; set; }
+
+        /// <summary>
+        /// The authenication scheme of the external registration.
+        /// </summary>
         public string ExternalRegistrationScheme => ExternalProviders?.SingleOrDefault()?.AuthenticationScheme;
     }
 }
