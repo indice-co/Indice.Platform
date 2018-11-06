a0a591a8b48eb3b5a3a9b8a02442431947d88de1
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2018-11-06 18:23:18 +0200

    basic authorization for admins

---
+++ src/Indice.AspNetCore.Identity/Authorization/AuthorizationPolicyExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Indice.Security;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Indice.AspNetCore.Identity.Authorization
+{
+    /// <summary>
+    /// Authorization policy extensions for <see cref="AuthorizationPolicyBuilder"/>
+    /// </summary>
+    public static class AuthorizationPolicyExtensions
+    {
+        /// <summary>
+        /// Set up the <paramref name="policyBuilder"/> to require an administrator.
+        /// </summary>
+        /// <param name="policyBuilder">The policy builder</param>
+        /// <returns></returns>
+        public static AuthorizationPolicyBuilder RequireAdmin(this AuthorizationPolicyBuilder policyBuilder) {
+            policyBuilder.RequireAuthenticatedUser();
+            policyBuilder.RequireAssertion(context => {
+                if (context.HasFailed) {
+                    return true;
+                }
+                return context.User.IsAdmin();
+            });
+            return policyBuilder;
+        }
+    }
+}
---
+++ src/Indice.AspNetCore.Identity/Authorization/PolicyNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indice.AspNetCore.Identity.Authorization
+{
+    /// <summary>
+    /// Basic set of Authorization policy names
+    /// </summary>
+    public class BasicPolicyNames
+    {
+        /// <summary>
+        /// Only a user marked as Admin in the User store or with a role assignment of the name Administrator
+        /// is allowed.
+        /// </summary>
+        public const string BeAdmin = nameof(BeAdmin);
+    }
+}
