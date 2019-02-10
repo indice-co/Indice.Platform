53538079a91148b3b8f6b43bc39514586762c383
Georgios Manoltzas <g.manoltzas@indice.gr>
2019-02-10 22:30:58 +0200

    Comments.

--- src/Indice.AspNetCore.Identity/Services/ExtendedUserClaimsPrincipalFactory.cs
+++ src/Indice.AspNetCore.Identity/Services/ExtendedUserClaimsPrincipalFactory.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 using IdentityModel;
 using Indice.AspNetCore.Identity.Models;
@@ -17,28 +15,28 @@ namespace Indice.AspNetCore.Identity.Services
     /// </summary>
     public class ExtendedUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<User, IdentityRole>
     {
-
         /// <summary>
-        /// Constructor for the extender user claims principal factory
+        /// Constructor for the extender user claims principal factory.
         /// </summary>
-        /// <param name="userManager"></param>
-        /// <param name="roleManager"></param>
-        /// <param name="optionsAccessor"></param>
+        /// <param name="userManager">The <see cref="UserManager{User}"/> to retrieve user information from.</param>
+        /// <param name="roleManager">The <see cref="RoleManager{IdentityRole}"/> to retrieve a user's roles from.</param>
+        /// <param name="optionsAccessor">The configured <see cref="IdentityOptions"/>.</param>
         public ExtendedUserClaimsPrincipalFactory(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IOptions<IdentityOptions> optionsAccessor)
-            : base(userManager, roleManager, optionsAccessor) {
-            
-        }
+            : base(userManager, roleManager, optionsAccessor) { }
 
         /// <summary>
         ///  Generate the claims for a user.
         /// </summary>
         /// <param name="user">The user to create a <see cref="ClaimsIdentity"/> from.</param>
-        /// <returns></returns>
+        /// <returns>The claim-based identity of the user.</returns>
         protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user) {
+            // https://github.com/aspnet/AspNetCore/blob/master/src/Identity/Extensions.Core/src/UserClaimsPrincipalFactory.cs#L135
             var identity = await base.GenerateClaimsAsync(user);
             var additionalClaims = new List<Claim>();
+
             if (!identity.HasClaim(x => x.Type == BasicClaimTypes.Admin)) {
                 var isAdmin = user.Admin;
+
                 if (!isAdmin) {
                     if (identity.HasClaim(x => x.Type == JwtClaimTypes.Role)) {
                         isAdmin = identity.HasClaim(JwtClaimTypes.Role, "Administrator");
@@ -47,8 +45,10 @@ namespace Indice.AspNetCore.Identity.Services
                         isAdmin = roles.Where(x => x.Value == "Administrator").Any();
                     }
                 }
+
                 additionalClaims.Add(new Claim(BasicClaimTypes.Admin, isAdmin.ToString().ToLower(), ClaimValueTypes.Boolean));
             }
+
             identity.AddClaims(additionalClaims);
             return identity;
         }
