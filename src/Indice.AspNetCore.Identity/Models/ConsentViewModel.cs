b65f43f208b3d81885750dace1991f1b60d419cf
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2019-05-20 15:42:10 +0300

    additions in consent view model

--- src/Indice.AspNetCore.Identity/Models/ConsentViewModel.cs
+++ src/Indice.AspNetCore.Identity/Models/ConsentViewModel.cs
@@ -3,17 +3,56 @@
 
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Indice.AspNetCore.Identity.Models
 {
+    /// <summary>
+    /// Consent view model
+    /// </summary>
     public class ConsentViewModel : ConsentInputModel
     {
+        /// <summary>
+        /// Client name
+        /// </summary>
         public string ClientName { get; set; }
+        /// <summary>
+        /// Client site
+        /// </summary>
         public string ClientUrl { get; set; }
+        /// <summary>
+        /// Logo url of the client
+        /// </summary>
         public string ClientLogoUrl { get; set; }
+
+        /// <summary>
+        /// Should the checkbox to allow remember be available
+        /// </summary>
         public bool AllowRememberConsent { get; set; }
 
+        /// <summary>
+        /// Identity scopes
+        /// </summary>
         public IEnumerable<ScopeViewModel> IdentityScopes { get; set; }
+
+        /// <summary>
+        /// Resource scopes
+        /// </summary>
         public IEnumerable<ScopeViewModel> ResourceScopes { get; set; }
+
+        /// <summary>
+        /// Available Sca Methods
+        /// </summary>
+        public IEnumerable<ScaMethodViewModel> ScaMethods { get; set; }
+
+        /// <summary>
+        /// Strong customer authentication code sent show the input.
+        /// </summary>
+        public bool ShowScaCodeInput { get; set; }
+
+        /// <summary>
+        /// Requires Strong customer authentication
+        /// </summary>
+        public bool RequiresSca => ResourceScopes?.Where(x => x.RequiresSca).Any() == true;
     }
 }
