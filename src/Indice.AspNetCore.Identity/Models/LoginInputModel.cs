2b24cfc93569e55316ef3ae2eb61845f93aa3899
Georgios Manoltzas <g.manoltzas@indice.gr>
2019-05-22 17:52:17 +0300

    Remove data annotations.

--- src/Indice.AspNetCore.Identity/Models/LoginInputModel.cs
+++ src/Indice.AspNetCore.Identity/Models/LoginInputModel.cs
@@ -1,18 +1,28 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-
-using System.ComponentModel.DataAnnotations;
-
 namespace Indice.AspNetCore.Identity.Models
 {
+    /// <summary>
+    /// Model used in login screen.
+    /// </summary>
     public class LoginInputModel
     {
-        [Required]
+        /// <summary>
+        /// The name of the user.
+        /// </summary>
         public string Username { get; set; }
-        [Required]
+        /// <summary>
+        /// User's password.
+        /// </summary>
         public string Password { get; set; }
+        /// <summary>
+        /// Determines whether the login will be remembered after user closes browser window.
+        /// </summary>
         public bool RememberLogin { get; set; }
+        /// <summary>
+        /// The URL to return.
+        /// </summary>
         public string ReturnUrl { get; set; }
     }
 }
