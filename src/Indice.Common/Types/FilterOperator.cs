59232101e6fa77d543b739206d09676bffe99995
Georgios Manoltzas <g.manoltzas@indice.gr>
2019-05-23 19:34:22 +0300

    Add some comments.

--- src/Indice.Common/Types/FilterOperator.cs
+++ src/Indice.Common/Types/FilterOperator.cs
@@ -1,13 +1,37 @@
 ﻿namespace Indice.Types
 {
+    /// <summary>
+    /// Represents an operator when querying for data.
+    /// </summary>
     public enum FilterOperator : short
     {
+        /// <summary>
+        /// Equal.
+        /// </summary>
         Eq = 0,
+        /// <summary>
+        /// Not equal
+        /// </summary>
         Neq,
+        /// <summary>
+        /// Greater than.
+        /// </summary>
         Gt,
+        /// <summary>
+        /// Less than.
+        /// </summary>
         Lt,
+        /// <summary>
+        /// Greater than or equal.
+        /// </summary>
         Gte,
+        /// <summary>
+        /// Less than or equal.
+        /// </summary>
         Lte,
+        /// <summary>
+        /// Contains.
+        /// </summary>
         Contains
     }
 }
