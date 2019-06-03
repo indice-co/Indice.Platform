3e8ee23c48556b8ca9e510d4ba1dcfe61a5212c8
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2019-06-03 19:06:27 +0300

    Indice hosting package

---
+++ src/Indice.Hosting/WorkerHostOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Hosting
+{
+    internal class WorkerHostOptions
+    {
+        public bool RunAsService { get; set; }
+    }
+}
