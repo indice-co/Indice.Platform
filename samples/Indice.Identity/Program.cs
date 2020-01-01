c9908a96237673ce286814b9b4ddb079399631de
Georgios Manoltzas <g.manoltzas@indice.gr>
2020-01-01 20:46:25 +0200

    Logging for sample.

--- samples/Indice.Identity/Program.cs
+++ samples/Indice.Identity/Program.cs
@@ -2,6 +2,10 @@
 using Indice.Extensions.Configuration.EFCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace Indice.Identity
 {
@@ -14,7 +18,20 @@ namespace Indice.Identity
         /// The entry point of the web application.
         /// </summary>
         /// <param name="args">Optional command line arguments.</param>
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args) {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try {
+                logger.Debug("Application initialization. Main method.");
+                CreateHostBuilder(args).Build().Run();
+            } catch (Exception exception) {
+                // NLog: catch setup errors.
+                logger.Error(exception, "Stopped program because of exception.");
+                throw;
+            } finally {
+                // Ensure to flush and stop internal timers/threads before application-exit (avoid segmentation fault on Linux).
+                LogManager.Shutdown();
+            }
+        }
 
         /// <summary>
         /// Buildes the web host.
@@ -25,6 +42,11 @@ namespace Indice.Identity
                 .ConfigureWebHostDefaults(webHostBuilder => {
                     webHostBuilder.UseStartup<Startup>();
                 })
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog()  // NLog: Setup NLog for dependency injection.
                 .UseEFConfiguration(reloadInterval: TimeSpan.FromHours(1), connectionStringName: "IdentityDb");
     }
 }
