a8743f33ec60d2903b7d0c8f77cf558aafdd12bf
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2020-01-15 16:21:27 +0200

    TypeConverterJsonAdapter

---
+++ src/Indice.Common/Serialization/TypeConverterAdapter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Indice.Serialization
+{
+    /// <summary>
+    /// Adapter between <see cref="System.ComponentModel.TypeConverter"/> and <see cref="JsonConverter"/>
+    /// </summary>
+    public class TypeConverterJsonAdapter : JsonConverter<object>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="typeToConvert"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public override object Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) {
+
+            var converter = TypeDescriptor.GetConverter(typeToConvert);
+            var text = reader.GetString();
+            return converter.ConvertFromString(text);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="objectToWrite"></param>
+        /// <param name="options"></param>
+        public override void Write(
+            Utf8JsonWriter writer,
+            object objectToWrite,
+            JsonSerializerOptions options) {
+
+            var converter = TypeDescriptor.GetConverter(objectToWrite);
+            var text = converter.ConvertToString(objectToWrite);
+            writer.WriteStringValue(text);
+        }
+        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="typeToConvert"></param>
+        /// <returns></returns>
+        public override bool CanConvert(Type typeToConvert) {
+            var hasConverter = typeToConvert.GetCustomAttributes<TypeConverterAttribute>(inherit: true).Any();
+            if (!hasConverter)
+                return false;
+            return true;
+        }
+    }
+}
--- test/Indice.Common.Tests/TextJsonTests.cs
+++ test/Indice.Common.Tests/TextJsonTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Indice.Serialization;
+using Indice.Types;
 using Xunit;
 
 namespace Indice.Common.Tests
@@ -21,6 +23,29 @@ namespace Indice.Common.Tests
             var doc = JsonDocument.Parse(expectedJson);
             var outputJson = JsonSerializer.Serialize(doc.RootElement);
             Assert.Equal(expectedJson, outputJson);
+        }
+        
+        [Fact]
+        public void RoundtripTypeConverterAdapter() {
+            var options = new JsonSerializerOptions();
+            // Support for OpenAPI / Swagger when using System.Text.Json is ongoing and unlikely to be available as part of the 3.0 release.
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.Converters.Add(new JsonStringEnumConverter());
+            options.Converters.Add(new TypeConverterJsonAdapter());
+            options.IgnoreNullValues = true;
+            var model = new TestTypeConverters { Point = GeoPoint.Parse("37.9888529,23.7037796"), Id = new Base64Id(Guid.Parse("04F39650-F015-4831-8592-D2D82E70589F")) };
+            var jsonExpected = "{\"point\":\"37.9888529,23.7037796\",\"id\":\"UJbzBBXwMUiFktLYLnBYnw\"}";
+            var json = JsonSerializer.Serialize(model, options);
+            Assert.Equal(jsonExpected, json);
+            var output = JsonSerializer.Deserialize<TestTypeConverters>(json, options);
+            Assert.Equal(model.Point.Latitude, output.Point.Latitude);
+            Assert.Equal(model.Id, output.Id);
+        }
+
+        class TestTypeConverters
+        {
+            public GeoPoint Point { get; set; }
+            public Base64Id Id { get; set; }
         }
     }
 }
