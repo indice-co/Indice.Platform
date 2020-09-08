c2d3b2aa3c2c75db36ce49b8f21f5cf1d60543a2
Constantinos Leftheris <c.leftheris@indice.gr>
2020-09-08 19:34:29 +0300

    CountryInfo and CurrencyInfo lookups

---
+++ test/Indice.Common.Tests/CurrencyInfoTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Indice.Globalization;
+using Xunit;
+
+namespace Indice.Common.Tests
+{
+    public class CurrencyInfoTests
+    {
+        public CurrencyInfoTests() {
+
+        }
+
+        [Trait("Tag", "Currency")]
+        [Theory]
+        [InlineData("EUR")]
+        [InlineData("USD")]
+        [InlineData("GBP")]
+        [InlineData("CAD")]
+        [InlineData("CHF")]
+        public void ByISOCodeValid_Test(string iso) {
+            if (iso?.Length > 3) {
+                Assert.Throws<ArgumentOutOfRangeException>(() => CurrencyInfo.GetByISOSymbol(iso));
+            } else {
+                var currency = CurrencyInfo.GetByISOSymbol(iso);
+                Assert.NotNull(currency);
+            }
+        }
+
+        [Trait("Tag", "Currency")]
+        [Theory]
+        [InlineData("DOLLAR")]
+        [InlineData("XDSFSDFS")]
+        [InlineData("eur")]
+        public void ByISOCodeInvalid_Test(string iso) {
+            Assert.Throws<ArgumentOutOfRangeException>(() => CurrencyInfo.GetByISOSymbol(iso));
+        }
+    }
+}
