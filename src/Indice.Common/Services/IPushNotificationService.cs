fc3c311220e8fe34f1a49aeb249f10e7353f6b88
Saehon <vaggelanos@yahoo.com>
2020-11-06 17:01:23 +0200

    remove return from comments

--- src/Indice.Common/Services/IPushNotificationService.cs
+++ src/Indice.Common/Services/IPushNotificationService.cs
@@ -16,14 +16,12 @@ namespace Indice.Services
         /// <param name="pnsHandle">Platform Notification Service(pns) obtained from client app</param>
         /// <param name="tags">Tags are used to route notifications to the correct set of device handles</param>
         /// <param name="devicePlatform">Client device platform</param>
-        /// <returns></returns>
         Task Register(string deviceId, string pnsHandle, IList<string> tags, DevicePlatform devicePlatform);
 
         /// <summary>
         /// Unregister device from receiving notifications
         /// </summary>
         /// <param name="deviceId">The device id to unregister</param>
-        /// <returns></returns>
         Task UnRegister(string deviceId);
 
         /// <summary>
@@ -32,7 +30,6 @@ namespace Indice.Services
         /// <param name="message">Message of notifications</param>
         /// <param name="tags">Tags are used to route notifications to the correct set of device handles</param>
         /// <param name="devicePlatform">Client device platform</param>
-        /// <returns></returns>
         Task SendAsync(string message, IList<string> tags, DevicePlatform devicePlatform);
     }
 }
