5fc8db3e18cb3c2b8942c552439730c4af52ff5f
Giorgos Manoltzas <giorgos.manoltzas@outlook.com>
2020-11-14 15:11:11 +0200

    Rename wrong property name.

--- src/Indice.Hosting/Tasks/Data/Models/DbScheduledTask.cs
+++ src/Indice.Hosting/Tasks/Data/Models/DbScheduledTask.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Text.Json;
 
 namespace Indice.Hosting.Tasks.Data
@@ -11,29 +9,29 @@ namespace Indice.Hosting.Tasks.Data
     public class DbScheduledTask
     {
         /// <summary>
-        /// The id
+        /// The id.
         /// </summary>
         public string Id { get; set; }
         /// <summary>
-        /// The worker id that run the task
+        /// The worker id that run the task.
         /// </summary>
         public string WorkerId { get; set; }
         /// <summary>
-        /// Task group
+        /// Task group.
         /// </summary>
         public string Group { get; set; }
         /// <summary>
-        /// Task name
+        /// Task name.
         /// </summary>
         public string Description { get; set; }
         /// <summary>
-        /// The type name
+        /// The type name.
         /// </summary>
         public string Type { get; set; }
         /// <summary>
         /// The date.
         /// </summary>
-        public DateTimeOffset Lastxecution { get; set; }
+        public DateTimeOffset LastExecution { get; set; }
         /// <summary>
         /// The date.
         /// </summary>
@@ -47,11 +45,11 @@ namespace Indice.Hosting.Tasks.Data
         /// </summary>
         public ScheduledTaskStatus Status { get; set; }
         /// <summary>
-        /// The errors
+        /// The errors.
         /// </summary>
         public string Errors { get; set; }
         /// <summary>
-        /// The payload
+        /// The payload.
         /// </summary>
         public string Payload { get; set; }
         /// <summary>
@@ -60,26 +58,23 @@ namespace Indice.Hosting.Tasks.Data
         public double Progress { get; set; }
 
         /// <summary>
-        /// Generate the dto for this <see cref="DbScheduledTask"/>
+        /// Generates the DTO for this <see cref="DbScheduledTask"/>.
         /// </summary>
-        /// <param name="options"></param>
-        /// <returns></returns>
-        public ScheduledTask<TState> ToModel<TState>(JsonSerializerOptions options = null) where TState : class {
-            return new ScheduledTask<TState> {
-                Id = Id,
-                Description = Description,
-                Group = Group,
-                Errors = Errors,
-                ExecutionCount = ExecutionCount,
-                Lastxecution = Lastxecution,
-                NextExecution = NextExecution,
-                Progress = Progress,
-                State = JsonSerializer.Deserialize<TState>(Payload, options ?? WorkerJsonOptions.GetDefaultSettings()),
-                Status = Status,
-                Type = Type,
-                WorkerId = WorkerId
-            };
-        }
+        /// <param name="options">Provides options to be used with <see cref="JsonSerializer"/>.</param>
+        public ScheduledTask<TState> ToModel<TState>(JsonSerializerOptions options = null) where TState : class => new ScheduledTask<TState> {
+            Id = Id,
+            Description = Description,
+            Group = Group,
+            Errors = Errors,
+            ExecutionCount = ExecutionCount,
+            LastExecution = LastExecution,
+            NextExecution = NextExecution,
+            Progress = Progress,
+            State = JsonSerializer.Deserialize<TState>(Payload, options ?? WorkerJsonOptions.GetDefaultSettings()),
+            Status = Status,
+            Type = Type,
+            WorkerId = WorkerId
+        };
 
         internal DbScheduledTask From<TState>(ScheduledTask<TState> model, JsonSerializerOptions options = null) where TState : class {
             Id = model.Id;
@@ -87,7 +82,7 @@ namespace Indice.Hosting.Tasks.Data
             Group = model.Group;
             Errors = model.Errors;
             ExecutionCount = model.ExecutionCount;
-            Lastxecution = model.Lastxecution;
+            LastExecution = model.LastExecution;
             NextExecution = model.NextExecution;
             Progress = model.Progress;
             Payload = JsonSerializer.Serialize(model.State, options ?? WorkerJsonOptions.GetDefaultSettings());
