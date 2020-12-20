302740b9e05a7e8f0c1c0f8af19821ea9894d99d
Giorgos Manoltzas <giorgos.manoltzas@outlook.com>
2020-12-20 14:47:10 +0200

    version bump

--- test/Indice.Common.Tests/ListOptionsTests.cs
+++ test/Indice.Common.Tests/ListOptionsTests.cs
@@ -33,30 +33,9 @@ namespace Indice.Common.Tests
     public class DocumentListFilter
     {
         public string Code { get; set; }
-        public string Number { get; set; }
-        public string ParentNumber { get; set; }
         public DateTime? From { get; set; }
-        public DateTime? To { get; set; }
-        public DateTime? PeriodFrom { get; set; }
-        public DateTime? PeriodTo { get; set; }
-        public DateTime? ModifiedFrom { get; set; }
-        public DateTime? ModifiedTo { get; set; }
-        public DateTime? DueFrom { get; set; }
-        public DateTime? DueTo { get; set; }
         public DocumentStatus[] Status { get; set; }
-        public RecordType? RecordType { get; set; }
-        public string RecipientCode { get; set; }
-        public string RecipientName { get; set; }
-        public Guid? RecipientId { get; set; }
-        public string RecipientOrganizationName { get; set; }
-        public string RecipientContactName { get; set; }
-        public Guid[] TypeId { get; set; }
-        public Guid[] ProductId { get; set; }
-        public string CustomerReference { get; set; }
-        public string PaymentCode { get; set; }
-        public string Title { get; set; }
         public bool? IsMarked { get; set; }
-        public bool? HasSyncError { get; set; }
     }
 
     public enum DocumentStatus : short
@@ -68,12 +47,5 @@ namespace Indice.Common.Tests
         Paid = 4,
         Void = 5,
         Deleted = 6
-    }
-
-    public enum RecordType : short
-    {
-        AccountsPayable = -1,
-        AccountsNeutral = 0,
-        AccountsReceivable = 1
     }
 }
