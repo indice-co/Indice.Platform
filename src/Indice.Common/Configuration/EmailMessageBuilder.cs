c46c324337bd5722cd5452c19d8f454f7bc75842
Constantinos Leftheris <c.leftheris@indice.gr>
2021-03-02 20:01:13 +0200

    email builder refactoring

--- src/Indice.Common/Configuration/EmailMessageBuilder.cs
+++ src/Indice.Common/Configuration/EmailMessageBuilder.cs
@@ -2,108 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Indice.Configuration
+namespace Indice.Services
 {
-    /// <summary>
-    /// A convinient builder to construct an instance of <see cref="EmailMessage{TModel}"/>.
-    /// </summary>
-    /// <typeparam name="TModel"></typeparam>
-    public class EmailMessageBuilder<TModel> : EmailMessageBuilder where TModel : class
-    {
-        /// <summary>
-        /// Data that are passed to the email template.
-        /// </summary>
-        public TModel Data { get; set; }
-
-        /// <summary>
-        /// Adds one or more recipients to the message.
-        /// </summary>
-        /// <param name="recipients">The email addresses of the recipients.</param>
-        /// <returns></returns>
-        public new EmailMessageBuilder<TModel> To(params string[] recipients) {
-            if (recipients?.Length == 0) {
-                throw new ArgumentException("One or more recipients must be declared for the message.", nameof(recipients));
-            }
-            foreach (var recipient in recipients.Distinct()) {
-                Recipients.Add(recipient);
-            }
-            return this;
-        }
-
-        /// <summary>
-        /// Defines the subject of the message.
-        /// </summary>
-        /// <param name="subject">The subject of the message.</param>
-        /// <returns></returns>
-        public new EmailMessageBuilder<TModel> WithSubject(string subject) {
-            if (string.IsNullOrEmpty(subject)) {
-                throw new ArgumentException("A subject for the message cannot be null or empty", nameof(subject));
-            }
-            Subject = subject;
-            return this;
-        }
-
-        /// <summary>
-        /// Defines the body of the message.
-        /// </summary>
-        /// <param name="body">The body of the message.</param>
-        /// <returns></returns>
-        public new EmailMessageBuilder<TModel> WithBody(string body) {
-            if (string.IsNullOrEmpty(body)) {
-                throw new ArgumentException("A body for the message cannot be null or empty", nameof(body));
-            }
-            Body = body;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds a model that is passed to the email template.
-        /// </summary>
-        /// <param name="data">Data that are passed to the email template.</param>
-        /// <returns></returns>
-        public EmailMessageBuilder<TModel> WithData(TModel data) {
-            Data = data;
-            return this;
-        }
-
-        /// <summary>
-        /// Defines the template used to render the email. Defaults to 'Email'. It has to be a Razor view, discoverable by the Razor Engine. (ex. Located in Views -> Shared folder).
-        /// </summary>
-        /// <param name="template">The template used to render the email. Defaults to 'Email'.</param>
-        /// <returns></returns>
-        public new EmailMessageBuilder<TModel> UsingTemplate(string template) {
-            if (string.IsNullOrEmpty(template)) {
-                throw new ArgumentException("A template name cannot be null or empty", nameof(template));
-            }
-            Template = template;
-            return this;
-        }
-
-        /// <summary>
-        /// Adds one or more attachments to the message. Attachments length cannot exceed 20 MB.
-        /// </summary>
-        /// <param name="attachments">Optional attachments contained in the message.</param>
-        /// <returns></returns>
-        public new EmailMessageBuilder<TModel> WithAttachments(params FileAttachment[] attachments) {
-            if (attachments?.Length == 0) {
-                throw new ArgumentException("One or more attachments must be declared for the message.", nameof(attachments));
-            }
-            Attachments = attachments;
-            return this;
-        }
-
-        /// <summary>
-        /// Returns the <see cref="EmailMessage{TModel}"/> instance made by the builder.
-        /// </summary>
-        /// <returns></returns>
-        public new EmailMessage<TModel> Build() => new EmailMessage<TModel>(Recipients, Subject, Body, Template, Data, Attachments);
-    }
-
+    
     /// <summary>
     /// A convinient builder to construct an instance of <see cref="EmailMessage"/>.
     /// </summary>
-    public class EmailMessageBuilder
-    {
+    public class EmailMessageBuilder {
         /// <summary>
         /// The email addresses of the recipients.
         /// </summary>
@@ -123,83 +28,112 @@ namespace Indice.Configuration
         /// The template used to render the email. Defaults to 'Email'.
         /// </summary>
         public string Template { get; set; } = "Email";
+        /// <summary>
+        /// Data that are passed to the email template.
+        /// </summary>
+        public object Data { get; set; }
 
         /// <summary>
         /// Optional attachments contained in the message.
         /// </summary>
         public IList<FileAttachment> Attachments { get; set; } = new List<FileAttachment>();
+    }
+
+    /// <summary>
+    /// <see cref="EmailMessageBuilder" /> extensions
+    /// </summary>
+    public static class EmailMessageBuilderExtensions
+    {
 
         /// <summary>
         /// Adds one or more recipients to the message.
         /// </summary>
+        /// <param name="builder">The builder</param>
         /// <param name="recipients">The email addresses of the recipients.</param>
         /// <returns></returns>
-        public EmailMessageBuilder To(params string[] recipients) {
+        public static EmailMessageBuilder To(this EmailMessageBuilder builder, params string[] recipients) {
             if (recipients?.Length == 0) {
                 throw new ArgumentException("One or more recipients must be declared for the message.", nameof(recipients));
             }
             foreach (var recipient in recipients.Distinct()) {
-                Recipients.Add(recipient);
+                builder.Recipients.Add(recipient);
             }
-            return this;
+            return builder;
         }
 
         /// <summary>
         /// Defines the subject of the message.
         /// </summary>
+        /// <param name="builder">The builder</param>
         /// <param name="subject">The subject of the message.</param>
         /// <returns></returns>
-        public EmailMessageBuilder WithSubject(string subject) {
+        public static EmailMessageBuilder WithSubject(this EmailMessageBuilder builder, string subject) {
             if (string.IsNullOrEmpty(subject)) {
                 throw new ArgumentException("A subject for the message cannot be null or empty", nameof(subject));
             }
-            Subject = subject;
-            return this;
+            builder.Subject = subject;
+            return builder;
         }
 
         /// <summary>
         /// Defines the body of the message.
         /// </summary>
+        /// <param name="builder">The builder</param>
         /// <param name="body">The body of the message.</param>
         /// <returns></returns>
-        public EmailMessageBuilder WithBody(string body) {
+        public static EmailMessageBuilder WithBody(this EmailMessageBuilder builder, string body) {
             if (string.IsNullOrEmpty(body)) {
                 throw new ArgumentException("A body for the message cannot be null or empty", nameof(body));
             }
-            Body = body;
-            return this;
+            builder.Body = body;
+            return builder;
         }
 
         /// <summary>
         /// Defines the template used to render the email. Defaults to 'Email'. It has to be a Razor view, discoverable by the Razor Engine. (ex. Located in Views -> Shared folder).
         /// </summary>
+        /// <param name="builder">The builder</param>
         /// <param name="template">The template used to render the email. Defaults to 'Email'.</param>
         /// <returns></returns>
-        public EmailMessageBuilder UsingTemplate(string template) {
+        public static EmailMessageBuilder UsingTemplate(this EmailMessageBuilder builder, string template) {
             if (string.IsNullOrEmpty(template)) {
                 throw new ArgumentException("A template name cannot be null or empty", nameof(template));
             }
-            Template = template;
-            return this;
+            builder.Template = template;
+            return builder;
         }
 
         /// <summary>
         /// Adds one or more attachments to the message. Attachments length cannot exceed 20 MB.
         /// </summary>
+        /// <param name="builder">The builder</param>
         /// <param name="attachments">Optional attachments contained in the message.</param>
         /// <returns></returns>
-        public EmailMessageBuilder WithAttachments(params FileAttachment[] attachments) {
+        public static EmailMessageBuilder WithAttachments(this EmailMessageBuilder builder, params FileAttachment[] attachments) {
             if (attachments?.Length == 0) {
                 throw new ArgumentException("One or more attachments must be declared for the message.", nameof(attachments));
             }
-            Attachments = attachments;
-            return this;
+            builder.Attachments = attachments;
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a model that is passed to the email template.
+        /// </summary>
+        /// <param name="builder">The builder</param>
+        /// <param name="data">Data that are passed to the email template.</param>
+        /// <returns></returns>
+        public static EmailMessageBuilder WithData<TModel>(this EmailMessageBuilder builder, TModel data) where TModel : class {
+            builder.Data = data;
+            return builder;
         }
 
         /// <summary>
         /// Returns the <see cref="EmailMessage"/> instance made by the builder.
         /// </summary>
+        /// <param name="builder">The builder</param>
         /// <returns></returns>
-        public EmailMessage Build() => new EmailMessage(Recipients, Subject, Body, Template, Attachments);
+        public static EmailMessage Build(this EmailMessageBuilder builder) => 
+            new EmailMessage(builder.Recipients, builder.Subject, builder.Body, builder.Template, builder.Data, builder.Attachments);
     }
 }
---
+++ test/Indice.Services.Tests/EmailTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Indice.Services.Tests
+{
+    public class EmailTests
+    {
+        [Fact]
+        public async Task EmailBuilderTest () {
+            IEmailService emailService = new EmailServiceMoq();
+            await emailService.SendAsync(x => x.WithSubject("This will blow your mind")
+                                         .WithBody("This is a strong <strong>body</strong>.")
+                                         .To("c.leftheris@indice.gr")
+                                         .UsingTemplate("MyFantasticTemplate")
+                                         .WithData(new {
+                                             FirstName = "Constantinos",
+                                             LastName = "Leftheris"
+                                         }));
+
+            Assert.True(true);
+
+        }
+    }
+}
