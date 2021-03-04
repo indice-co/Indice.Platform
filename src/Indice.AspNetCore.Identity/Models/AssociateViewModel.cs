e9bf5e3dd6378d47adf4c84c21c685497536b577
Georgios Manoltzas <g.manoltzas@indice.gr>
2021-03-05 00:27:59 +0200

    add 2 new models

---
+++ src/Indice.AspNetCore.Identity/Models/AssociateViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Indice.AspNetCore.Identity.Models
+{
+    public class AssociateViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool HasAcceptedTerms { get; set; }
+        public bool HasReadPrivacyPolicy { get; set; }
+        public string Provider { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ src/Indice.AspNetCore.Identity/Models/SetPasswordModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Indice.AspNetCore.Identity.Models
+{
+    public class SetPasswordModel
+    {
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
