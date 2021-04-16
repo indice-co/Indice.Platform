c1623aab2816afacecf2293229ce86acb0f93a63
Constantinos Leftheris <c.leftheris@indice.gr>
2021-04-16 13:09:34 +0300

    some more comments

--- src/Indice.Common/Serialization/JsonUnsupportedTypeConverter.cs
+++ src/Indice.Common/Serialization/JsonUnsupportedTypeConverter.cs
@@ -8,13 +8,16 @@ namespace Indice.Serialization
     /// This converter is a NO Op conveter for the type <typeparamref name="TIgnore"/>
     /// </summary>
     /// <typeparam name="TIgnore">The type to ignore when serializing</typeparam>
+    /// <remarks>
+    /// A Json converter that skips serialization for a given type instead of throwing the unsupported exception. Very handy when exposing wcf proxy entities to the web directly 
+    /// - which you shouldnt be doing anyways ¯\_(ツ)_/¯
+    /// </remarks>
     public class JsonUnsupportedTypeConverter<TIgnore> : JsonConverter<TIgnore>
     {
         /// <inheritdoc/>
         public override TIgnore Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
             return default;
         }
-
 
         /// <inheritdoc/>
         public override void Write(Utf8JsonWriter writer, TIgnore value, JsonSerializerOptions options) {
