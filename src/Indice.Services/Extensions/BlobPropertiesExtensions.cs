18fa96c9117a14c8bc4b113c01ca85216477e4ba
vsyk <v.sykiotakis@indice.gr>
2021-10-05 11:49:44 +0300

    add todo and comments for etag extension method

--- src/Indice.Services/Extensions/BlobPropertiesExtensions.cs
+++ src/Indice.Services/Extensions/BlobPropertiesExtensions.cs
@@ -12,9 +12,10 @@ namespace Indice.Extensions
         /// Wraps eTag with quotes if not already wrapped.
         /// This a workaround for https://github.com/Azure/azure-sdk-for-net/issues/22877 
         /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="value">Etag to wrap with quotes if needed</param>
+        /// <returns>Etag string wrapped with quotes</returns>
         public static string GetHttpSafeETag(this ETag value) {
+            //TODO Remove when https://github.com/Azure/azure-sdk-for-net/issues/22877 is fixed
             var eTag = value.ToString();
             if (!string.IsNullOrEmpty(eTag)) {
                 if (!(eTag.StartsWith("\"") || eTag.StartsWith("W"))) {
