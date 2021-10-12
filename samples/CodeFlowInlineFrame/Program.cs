fab58c375ebcdc13bf4ebef679f3e0423b62b8de
Giorgos Manoltzas <g.manoltzas@indice.gr>
2021-10-12 19:32:49 +0300

    update sample project

--- samples/CodeFlowInlineFrame/Program.cs
+++ samples/CodeFlowInlineFrame/Program.cs
@@ -5,14 +5,12 @@ namespace CodeFlowInlineFrame
 {
     public class Program
     {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
+                .ConfigureWebHostDefaults(webHostBuilder => {
+                    webHostBuilder.UseStartup<Startup>();
                 });
     }
 }
