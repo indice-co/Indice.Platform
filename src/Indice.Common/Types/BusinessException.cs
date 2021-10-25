2cdd087045876f4a606a123015c7edfa4538d5b7
Georgios Manoltzas <g.manoltzas@indice.gr>
2021-10-25 23:15:07 +0300

    add BusinessException type - version bump

---
+++ src/Indice.Common/Types/BusinessException.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Indice.Types
+{
+    /// <summary>
+    /// Used to propagate business information upwards through the application layers.
+    /// </summary>
+    public class BusinessException : Exception
+    {
+        /// <summary>
+        /// Creates a new business exception.
+        /// </summary>
+        public BusinessException() : base() { }
+
+        /// <summary>
+        /// Creates a new business exception with the specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public BusinessException(string message) : base(message) { }
+
+        /// <summary>
+        /// Creates a new business exception with the specified error message and a list of errors.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="originOrCode">The source of the problem.</param>
+        /// <param name="errors">A list of errors.</param>
+        public BusinessException(string message, string originOrCode, IEnumerable<string> errors = null) : base(message) {
+            Code = originOrCode;
+            if (errors != null) {
+                Errors.Add(originOrCode, errors.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// The list of errors.
+        /// </summary>
+        public Dictionary<string, string[]> Errors { get; } = new Dictionary<string, string[]>();
+        /// <summary>
+        /// Error code.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
