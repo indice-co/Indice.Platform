30275b8ef45780c33689f1abb3f2dd43c0889313
c.leftheris <c.leftheris@live.com>
2021-11-25 20:00:13 +0200

    removed Timespan serializer from defaults in dotnet 6

--- src/Indice.Common/Serialization/JsonSerializerDefaults.cs
+++ src/Indice.Common/Serialization/JsonSerializerDefaults.cs
@@ -23,8 +23,11 @@ namespace Indice.Serialization
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
             options.Converters.Add(new JsonStringEnumConverter());
-            options.Converters.Add(new JsonTimeSpanConverter());
+#if NET6_0_OR_GREATER
+#else
+            options.Converters.Add(new JsonTimeSpanConverter());      
             options.Converters.Add(new JsonNullableTimeSpanConverter());
+#endif
             options.Converters.Add(new TypeConverterJsonAdapterFactory());
             options.Converters.Add(new ValueTupleJsonConverterFactory());
             options.Converters.Add(new JsonObjectToInferredTypeConverter());
