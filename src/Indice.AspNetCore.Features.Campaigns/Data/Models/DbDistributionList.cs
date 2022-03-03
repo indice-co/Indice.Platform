157cac2d647a4a4d94ca5ed4caeb5e81a11e7489
Georgios Manoltzas <g.manoltzas@indice.gr>
2022-03-03 18:43:37 +0200

    Create some more database models.

---
+++ src/Indice.AspNetCore.Features.Campaigns/Data/Models/DbDistributionList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Indice.AspNetCore.Features.Campaigns.Data.Models
+{
+    internal class DbDistributionList
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
