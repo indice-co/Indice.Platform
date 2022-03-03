ab4f564db761386cc6dcba5fe9700ce479cff4e8
c.leftheris <c.leftheris@live.com>
2022-03-03 18:45:01 +0200

    campaign contact

---
+++ src/Indice.AspNetCore.Features.Campaigns/Data/Mappings/DbContactMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indice.AspNetCore.Features.Campaigns.Data.Mappings
+{
+    internal class DbContactMap
+    {
+    }
+}
---
+++ src/Indice.AspNetCore.Features.Campaigns/Data/Models/DbContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indice.AspNetCore.Features.Campaigns.Data.Models
+{
+    internal class DbContact
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string RecipientId { get; set; }
+        public string Salute { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DeviceId { get; set; }
+        public Guid DistributionListId { get; set; }
+    }
+}
