dfc98199771d4a6bf01aca8aada8257964561e02
Georgios Manoltzas <g.manoltzas@indice.gr>
2022-04-18 16:01:14 +0300

    Helper method for getting NotificationsManager in Functions project

---
+++ src/Indice.Features.Messages.Worker.Azure/FunctionContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Indice.Features.Messages.Core.Manager;
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    /// <summary>
+    /// Extension methods on <see cref="FunctionContext"/> class.
+    /// </summary>
+    public static class FunctionContextExtensions
+    {
+        /// <summary>
+        /// Gets the registered <see cref="NotificationsManager"/> instance from the DI container.
+        /// </summary>
+        /// <param name="executionContext">Encapsulates the information about a function execution.</param>
+        public static NotificationsManager GetNotificationsManager(this FunctionContext executionContext) =>
+            (NotificationsManager)executionContext.InstanceServices.GetService(typeof(NotificationsManager));
+    }
+}
