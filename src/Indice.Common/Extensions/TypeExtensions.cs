54836578c702a1dbab35eaa0bb961cda306c6555
Georgios Manoltzas <g.manoltzas@indice.gr>
2022-07-11 20:00:20 +0300

    Pass a concrete type to the view for rendering engine

--- src/Indice.Common/Extensions/TypeExtensions.cs
+++ src/Indice.Common/Extensions/TypeExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using NJsonSchema;
@@ -12,20 +14,34 @@ namespace Indice.Extensions
     {
         /// <summary>Determines whether a type is decorated with <see cref="FlagsAttribute"/>.</summary>
         /// <param name="type">The type to check.</param>
-        public static bool IsFlagsEnum(this Type type) => 
-            (type.IsEnum && type.GetCustomAttributes(typeof(FlagsAttribute), inherit: false).Any()) || 
+        public static bool IsFlagsEnum(this Type type) =>
+            (type.IsEnum && type.GetCustomAttributes(typeof(FlagsAttribute), inherit: false).Any()) ||
             (Nullable.GetUnderlyingType(type)?.IsEnum == true && Nullable.GetUnderlyingType(type).GetCustomAttributes(typeof(FlagsAttribute), inherit: false).Any());
 
         /// <summary>Generates JSON schema for a given C# class.</summary>
         /// <param name="type">Class type</param>
         /// <returns>A string containing JSON schema for a given class type.</returns>
         public static string ToJsonSchema(this Type type) {
-            var schema = JsonSchema.FromType(type, new JsonSchemaGeneratorSettings { 
-                SerializerSettings = new JsonSerializerSettings { 
+            var schema = JsonSchema.FromType(type, new JsonSchemaGeneratorSettings {
+                SerializerSettings = new JsonSerializerSettings {
                     ContractResolver = new CamelCasePropertyNamesContractResolver()
                 }
             });
             return schema.ToJson();
+        }
+
+        /// <summary>Determines whether a type is anonymous.</summary>
+        /// <param name="type"></param>
+        /// <returns>Returns true if type is anonymous, otherwise false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
+        public static bool IsAnonymousType(this Type type) {
+            if (type == null) {
+                throw new ArgumentNullException(nameof(type), "Parameter type cannot be null.");
+            }
+            return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
+                && type.IsGenericType && type.Name.Contains("AnonymousType")
+                && (type.Name.StartsWith("<>") || type.Name.StartsWith("VB$"))
+                && type.Attributes.HasFlag(TypeAttributes.NotPublic);
         }
     }
 }
---
+++ test/Indice.AspNetCore.Views/Models/EmailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Indice.AspNetCore.Views.Models
+{
+    public class EmailModel
+    {
+        public string Salutation { get; set; }
+        public string FullName { get; set; }
+        public string Message { get; set; }
+    }
+}
