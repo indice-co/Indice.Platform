069562c5982d043b1dffdee826d1b6f7642b4e13
c.asvestopoulos@indice.gr <c.asvestopoulos@indice.gr>
2022-09-22 14:00:20 +0300

    add GetCaseActivity

---
+++ src/Indice.Features.Cases.AspNetCore/Workflows/Activities/GetCaseActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Elsa;
+using Elsa.ActivityResults;
+using Elsa.Attributes;
+using Elsa.Services.Models;
+using Indice.Features.Cases.Interfaces;
+
+namespace Indice.Features.Cases.Workflows.Activities
+{
+    [Activity(
+        Category = "Cases",
+        DisplayName = "Get Case",
+        Description = "Get Case.",
+        Outcomes = new[] { OutcomeNames.Done, "Failed" }
+    )]
+    internal class GetCaseActivity : BaseCaseActivity
+    {
+        private readonly IAdminCaseService _adminCaseService;
+
+        public GetCaseActivity(
+            IAdminCaseMessageService caseMessageService,
+            IAdminCaseService adminCaseService)
+            : base(caseMessageService) {
+            _adminCaseService = adminCaseService ?? throw new ArgumentNullException(nameof(adminCaseService));
+        }
+
+        [ActivityOutput]
+        public object Output { get; set; }
+
+        public override async ValueTask<IActivityExecutionResult> ExecuteAsync(ActivityExecutionContext context) {
+            CaseId ??= Guid.Parse(context.CorrelationId);
+            // Run as systemic user, since this is a system activity for creating conditions at workflow
+            var systemUser = Cases.Extensions.PrincipalExtensions.SystemUser();
+            var @case = await _adminCaseService.GetCaseById(systemUser, CaseId.Value!);
+            Output = @case;
+            context.LogOutputProperty(this, nameof(Output), Output);
+            return Done(Output);
+        }
+    }
+}
