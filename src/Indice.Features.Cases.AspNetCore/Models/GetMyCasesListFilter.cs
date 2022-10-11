3524d02a911fec6f9d638e67d5e7de4a944748b3
c.asvestopoulos@indice.gr <c.asvestopoulos@indice.gr>
2022-10-11 15:40:58 +0300

    add various filters

--- src/Indice.Features.Cases.AspNetCore/Models/GetMyCasesListFilter.cs
+++ src/Indice.Features.Cases.AspNetCore/Models/GetMyCasesListFilter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Indice.Features.Cases.Data.Models;
 
 namespace Indice.Features.Cases.Models
@@ -13,8 +14,28 @@ namespace Indice.Features.Cases.Models
         /// </summary>
         public IEnumerable<string>? CaseTypeTags { get; set; }
         /// <summary>
-        /// The current status of the case.
+        /// The case status filter.
         /// </summary>
         public List<CasePublicStatus>? PublicStatuses { get; set; }
+        /// <summary>
+        /// The case type code filter.
+        /// </summary>
+        public List<string>? CaseTypeCodes { get; set; }
+        /// <summary>
+        /// The CreatedFrom filter.
+        /// </summary>
+        public DateTime? CreatedFrom { get; set; }
+        /// <summary>
+        /// The CreatedTo filter.
+        /// </summary>
+        public DateTime? CreatedTo { get; set; }
+        /// <summary>
+        /// The CompletedFrom filter.
+        /// </summary>
+        public DateTime? CompletedFrom { get; set; }
+        /// <summary>
+        /// The CompletedTo filter.
+        /// </summary>
+        public DateTime? CompletedTo { get; set; }
     }
 }
