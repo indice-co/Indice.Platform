5a1e923967010034bf6e461cc70d333971d3959f
Georgios Manoltzas <g.manoltzas@indice.gr>
2022-10-19 00:01:26 +0300

    Fix

--- src/Indice.AspNetCore.Identity/Services/SecurityTokenBasedTotpService.cs
+++ src/Indice.AspNetCore.Identity/Services/SecurityTokenBasedTotpService.cs
@@ -1,7 +1,12 @@
-﻿namespace Indice.AspNetCore.Identity
+﻿using System;
+
+namespace Indice.AspNetCore.Identity
 {
     /// <summary></summary>
     public sealed class SecurityTokenBasedTotpService : TotpServiceBase
     {
+        /// <summary>Creates a new instance of <see cref="SecurityTokenBasedTotpService"/>.</summary>
+        /// <param name="serviceProvider">Defines a mechanism for retrieving a service object; that is, an object that provides custom support to other objects.</param>
+        public SecurityTokenBasedTotpService(IServiceProvider serviceProvider) : base(serviceProvider) { }
     }
 }
