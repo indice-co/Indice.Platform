b5401b7bb2f07d466687d8b1bc6e240aadc88563
Dimitris Karkanas <d.karkanas@gmail.com>
2022-11-08 11:51:42 +0200

    add proper comments in CaseMessageStartedEvent

--- src/Indice.Features.Cases.AspNetCore/Events/CaseMessageCreatedEvent.cs
+++ src/Indice.Features.Cases.AspNetCore/Events/CaseMessageCreatedEvent.cs
@@ -15,15 +15,15 @@ namespace Indice.Features.Cases.Events
         public Guid CaseId { get; set; }
         
         /// <summary>
-        /// The <see cref="Message"/> that has been sent to the case service.
+        /// The <see cref="Message"/> that will be sent to the case service.
         /// </summary>
         public Message Message { get; set; }
 
         /// <summary>
-        /// Construct a new <see cref="CaseMessageSentEvent"/>.
+        /// Construct a new <see cref="CaseMessageCreatedEvent"/>.
         /// </summary>
         /// <param name="caseId">The Id of the case.</param>
-        /// <param name="message">The <see cref="Message"/> that has been sent to the case service.</param>
+        /// <param name="message">The <see cref="Message"/> that will be sent to the case service.</param>
         public CaseMessageCreatedEvent(Guid caseId, Message message) {
             CaseId = caseId;
             Message = message;
