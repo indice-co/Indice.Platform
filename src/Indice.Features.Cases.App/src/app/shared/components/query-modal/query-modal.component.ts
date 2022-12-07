3f6d2a8eac47ae87cdedf01b15412e1c90124cbb
c.asvestopoulos@indice.gr <c.asvestopoulos@indice.gr>
2022-12-07 10:59:17 +0200

    PR feedback

--- src/Indice.Features.Cases.App/src/app/shared/components/query-modal/query-modal.component.ts
+++ src/Indice.Features.Cases.App/src/app/shared/components/query-modal/query-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Modal } from '@indice/ng-components';
 import { CasesApiService, SaveQueryRequest } from 'src/app/core/services/cases-api.service';
 
@@ -8,22 +9,27 @@ import { CasesApiService, SaveQueryRequest } from 'src/app/core/services/cases-a
 })
 export class QueriesModalComponent implements OnInit {
   public friendlyName: string | undefined;
+  public queryParameters: string | undefined;
 
   constructor(
+    private _route: ActivatedRoute,
     private _api: CasesApiService,
     private modal: Modal
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // get a snapshot of queryParam
+    let paramMap = this._route.snapshot.queryParamMap;
+    // re-construct queryParameters to ensure that no user-added params are send to server & page will always be 1!
+    this.queryParameters = `?view=${paramMap.get('view')}&page=1&pagesize=${paramMap.get('pagesize')}&search=${paramMap.get('search')}&sort=${paramMap.get('sort')}&dir=${paramMap.get('dir')}&filter=${paramMap.get('filter')}`
+  }
 
   public closeModal() {
     this.modal.hide();
   }
 
   public saveQuery() {
-    let href = window.location.href;
-    let queryParameters = href.substring(href.indexOf('?'))
-    this._api.saveQuery(undefined, new SaveQueryRequest({ friendlyName: this.friendlyName, parameters: queryParameters }))
+    this._api.saveQuery(undefined, new SaveQueryRequest({ friendlyName: this.friendlyName, parameters: this.queryParameters }))
       .subscribe(
         (_ => this.modal.hide())
       )
