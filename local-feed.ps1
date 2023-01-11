c1c9882cdbdb6c1b81f7b0e6d1d1ce106f92d408
Constantinos Leftheris <c.leftheris@live.com>
2023-01-11 18:53:37 +0200

    local feed

--- local-feed.ps1
+++ local-feed.ps1
@@ -1,9 +1,12 @@
 # download nuget.exe
-If (!(Test-Path -Path .\nuget.exe )) {
-    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\nuget.exe
+If (!(Test-Path -Path ".\.nuget\nuget.exe" )) {
+    If (!(Test-Path -Path ".\.nuget" )) {
+        New-Item -ItemType "directory" -Path ".\.nuget"
+    }
+    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile ".\.nuget\nuget.exe"
 }
 # run the pack command to create packages
 & "$PSScriptRoot\pack.ps1"
 
 # create local feed
-.\nuget.exe init .\artifacts .\packages
+.\.nuget\nuget.exe init .\artifacts .\.nuget\packages
