b8543dfa54374d41db3cc10b6d9106ac3e631d87
Georgios Manoltzas <g.manoltzas@indice.gr>
2023-02-06 21:35:28 +0200

    Improved flow for release in NuGet through GitHub actions.

--- src/Indice.Features.Cases.UI/CasesUIMiddlewareExtensions.cs
+++ src/Indice.Features.Cases.UI/CasesUIMiddlewareExtensions.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using Indice.AspNetCore.EmbeddedUI;
+﻿using Indice.AspNetCore.EmbeddedUI;
 using Indice.Features.Cases.UI;
 
 namespace Microsoft.AspNetCore.Builder
 {
-    /// <summary>
-    /// Extension methods on <see cref="IApplicationBuilder"/>, used to register the <see cref="SpaUIMiddleware{TOptions}"/> middleware.
-    /// </summary>
+    /// <summary>Extension methods on <see cref="IApplicationBuilder"/>, used to register the <see cref="SpaUIMiddleware{TOptions}"/> middleware.</summary>
     /// <example>https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/write?view=aspnetcore-5.0#middleware-extension-method</example>
     public static class CasesUIMiddlewareExtensions
     {
-        /// <summary>
-        /// Registers the Cases UI single page application, using the provided options.
-        /// </summary>
+        /// <summary>Registers the Cases UI single page application, using the provided options.</summary>
         /// <param name="builder">Defines a class that provides the mechanisms to configure an application's request pipeline.</param>
         /// <param name="optionsAction">Options for configuring <see cref="SpaUIMiddleware{TOptions}"/> middleware.</param>
         public static IApplicationBuilder UseCasesUI(this IApplicationBuilder builder, Action<CasesUIOptions> optionsAction = null) =>
-            builder.UseSpaUI("cases-ui-dist", typeof(CasesUIMiddlewareExtensions).Assembly, optionsAction);
+            builder.UseSpaUI("cases-app", typeof(CasesUIMiddlewareExtensions).Assembly, optionsAction);
     }
 }
