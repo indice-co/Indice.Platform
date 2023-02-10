6c916efd4c2d8dbae267d317122343178f86b715
c.asvestopoulos@indice.gr <c.asvestopoulos@indice.gr>
2023-02-10 15:23:14 +0200

    add cache

--- src/Indice.Features.Cases.AspNetCore/Services/AdminReportService.cs
+++ src/Indice.Features.Cases.AspNetCore/Services/AdminReportService.cs
@@ -4,6 +4,7 @@ using Indice.Features.Cases.Data.Models;
 using Indice.Features.Cases.Interfaces;
 using Indice.Features.Cases.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using static Indice.Features.Cases.CasesApiConstants;
 
 namespace Indice.Features.Cases.Services
@@ -11,10 +12,15 @@ namespace Indice.Features.Cases.Services
     internal class AdminReportService : IAdminReportService
     {
         private readonly CasesDbContext _dbContext;
+        private readonly IDistributedCache _distributedCache;
+        private readonly string _caseReportCacheKey = $"{nameof(AdminReportService)}.caseReport";
 
         public AdminReportService(
-            CasesDbContext dbContext) {
+            CasesDbContext dbContext,
+            IDistributedCache distributedCache
+            ) {
             _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
         }
 
         //public async Task<List<GroupByReportResult>> GetCasesGroupedByCaseType() {
@@ -100,35 +106,65 @@ namespace Indice.Features.Cases.Services
         public async Task<List<GroupByReportResult>> GenerateReport(ReportTag reportTag) {
             switch (reportTag) {
                 case ReportTag.GroupedByCasetype:
-                    return await RunQuery(
-                        x => true,
-                        x => x.CaseType.Code,
-                        group => new GroupByReportResult { Label = group.Key, Count = group.Count() });
+                    return await _distributedCache.TryGetAndSetAsync(
+                        cacheKey: $"{_caseReportCacheKey}.{ReportTag.GroupedByCasetype}",
+                        getSourceAsync: async () => await RunQuery(
+                            x => true,
+                            x => x.CaseType.Code,
+                            group => new GroupByReportResult { Label = group.Key, Count = group.Count() }),
+                        options: new DistributedCacheEntryOptions {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                        });
                 case ReportTag.AgentGroupedByCasetype:
-                    return await RunQuery(
-                        x => x.Channel == Channels.Agent,
-                        x => x.CaseType.Code,
-                        group => new GroupByReportResult { Label = group.Key, Count = group.Count() });
+                    return await _distributedCache.TryGetAndSetAsync(
+                        cacheKey: $"{_caseReportCacheKey}.{ReportTag.AgentGroupedByCasetype}",
+                        getSourceAsync: async () => await RunQuery(
+                            x => x.Channel == Channels.Agent,
+                            x => x.CaseType.Code,
+                            group => new GroupByReportResult { Label = group.Key, Count = group.Count() }),
+                        options: new DistributedCacheEntryOptions {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                        });
                 case ReportTag.CustomerGroupedByCasetype:
-                    return await RunQuery(
-                        x => x.Channel == Channels.Customer,
-                        x => x.CaseType.Code,
-                        group => new GroupByReportResult { Label = group.Key, Count = group.Count() });
+                    return await _distributedCache.TryGetAndSetAsync(
+                        cacheKey: $"{_caseReportCacheKey}.{ReportTag.CustomerGroupedByCasetype}",
+                        getSourceAsync: async () => await RunQuery(
+                            x => x.Channel == Channels.Customer,
+                            x => x.CaseType.Code,
+                            group => new GroupByReportResult { Label = group.Key, Count = group.Count() }),
+                        options: new DistributedCacheEntryOptions {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                        });
                 case ReportTag.GroupedByStatus:
-                    return await RunQuery(
-                        x => true,
-                        x => x.Checkpoint.CheckpointType.Status,
-                        group => new GroupByReportResult { Label = group.Key.ToString(), Count = group.Count() });
+                    return await _distributedCache.TryGetAndSetAsync(
+                        cacheKey: $"{_caseReportCacheKey}.{ReportTag.GroupedByStatus}",
+                        getSourceAsync: async () => await RunQuery(
+                            x => true,
+                            x => x.Checkpoint.CheckpointType.Status,
+                            group => new GroupByReportResult { Label = group.Key.ToString(), Count = group.Count() }),
+                        options: new DistributedCacheEntryOptions {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                        });
                 case ReportTag.AgentGroupedByStatus:
-                    return await RunQuery(
-                        x => x.Channel == Channels.Agent,
-                        x => x.Checkpoint.CheckpointType.Status,
-                        group => new GroupByReportResult { Label = group.Key.ToString(), Count = group.Count() });
+                    return await _distributedCache.TryGetAndSetAsync(
+                        cacheKey: $"{_caseReportCacheKey}.{ReportTag.AgentGroupedByStatus}",
+                        getSourceAsync: async () => await RunQuery(
+                            x => x.Channel == Channels.Agent,
+                            x => x.Checkpoint.CheckpointType.Status,
+                            group => new GroupByReportResult { Label = group.Key.ToString(), Count = group.Count() }),
+                        options: new DistributedCacheEntryOptions {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                        });
                 case ReportTag.CustomerGroupedByStatus:
-                    return await RunQuery(
+                    return await _distributedCache.TryGetAndSetAsync(
+                        cacheKey: $"{_caseReportCacheKey}.{ReportTag.CustomerGroupedByStatus}",
+                        getSourceAsync: async () => await RunQuery(
                         x => x.Channel == Channels.Customer,
                         x => x.Checkpoint.CheckpointType.Status,
-                        group => new GroupByReportResult { Label = group.Key.ToString(), Count = group.Count() });
+                        group => new GroupByReportResult { Label = group.Key.ToString(), Count = group.Count() }),
+                        options: new DistributedCacheEntryOptions {
+                            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                        });
                 default:
                     throw new ArgumentException(nameof(reportTag));
             }
