d733a5c33787a36dd7f974bd1aaab3579d4686e2
Constantinos Leftheris <c.leftheris@live.com>
2023-03-23 20:24:31 +0200

    added ProducesProblem helpers on the RouteGroupBuilder

--- src/Indice.AspNetCore/Http/Extensions/OpenApiExtensions.cs
+++ src/Indice.AspNetCore/Http/Extensions/OpenApiExtensions.cs
@@ -1,4 +1,8 @@
 ﻿#if NET7_0_OR_GREATER
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.OpenApi.Models;
 
 namespace Microsoft.AspNetCore.Builder;
@@ -29,5 +33,26 @@ public static class OpenApiExtensions
             }
         });
     }
+    /// <summary>
+    /// Adds an <see cref="IProducesResponseTypeMetadata"/> with a <see cref="ProblemDetails"/> type
+    /// to <see cref="EndpointBuilder.Metadata"/> for all endpoints produced by <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="RouteGroupBuilder"/>.</param>
+    /// <param name="statusCode">The response status code.</param>
+    /// <param name="contentType">The response content type. Defaults to "application/problem+json".</param>
+    /// <returns>A <see cref="RouteGroupBuilder"/> that can be used to further customize the endpoint.</returns>
+    public static RouteGroupBuilder ProducesProblem(this RouteGroupBuilder builder, int statusCode, string? contentType = null) => 
+        builder.WithMetadata(new ProducesResponseTypeMetadata(typeof(ProblemDetails), statusCode, contentType ?? "application/problem+json"));
+
+    /// <summary>
+    /// Adds an <see cref="IProducesResponseTypeMetadata"/> with a <see cref="HttpValidationProblemDetails"/> type
+    /// to <see cref="EndpointBuilder.Metadata"/> for all endpoints produced by <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="RouteGroupBuilder"/>.</param>
+    /// <param name="statusCode">The response status code. Defaults to <see cref="StatusCodes.Status400BadRequest"/>.</param>
+    /// <param name="contentType">The response content type. Defaults to "application/problem+json".</param>
+    /// <returns>A <see cref="RouteGroupBuilder"/> that can be used to further customize the endpoint.</returns>
+    public static RouteGroupBuilder ProducesValidationProblem(this RouteGroupBuilder builder, int statusCode = 400, string? contentType = null) 
+        => builder.WithMetadata(new ProducesResponseTypeMetadata(typeof(HttpValidationProblemDetails), statusCode, contentType ?? "application/problem+json"));
 }
 #endif
--- src/Indice.AspNetCore/Http/Filters/ValidationFilter.cs
+++ src/Indice.AspNetCore/Http/Filters/ValidationFilter.cs
@@ -14,7 +14,7 @@ namespace Microsoft.AspNetCore.Builder;
 /// <summary>
 /// Model state validation convention extensions
 /// </summary>
-public static class ValidationFilterExtensions
+public static partial class ValidationFilterExtensions
 {
     /// <summary>
     /// Adds the validation of input parameters and <see cref="HttpValidationProblemDetails"/> automatic response when something is out of place.
@@ -126,20 +126,6 @@ public static class ValidationFilterExtensions
         });
 
         return builder;
-    }
-
-    // Equivalent to the .Produces call to add metadata to endpoints
-    private sealed class ProducesResponseTypeMetadata : IProducesResponseTypeMetadata
-    {
-        public ProducesResponseTypeMetadata(Type type, int statusCode, string contentType) {
-            Type = type;
-            StatusCode = statusCode;
-            ContentTypes = new[] { contentType };
-        }
-
-        public Type Type { get; }
-        public int StatusCode { get; }
-        public IEnumerable<string> ContentTypes { get; }
     }
 }
 #endif
