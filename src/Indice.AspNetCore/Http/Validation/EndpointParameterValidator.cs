277596be336d6c882d0009e49b7819e94e7fb709
Constantinos Leftheris <c.leftheris@live.com>
2023-03-23 19:40:01 +0200

    added more extension methods

---
+++ src/Indice.AspNetCore/Http/Validation/EndpointParameterValidator.cs
@@ -0,0 +1,22 @@
+﻿using MiniValidation;
+
+namespace Indice.AspNetCore.Http.Validation;
+
+/// <summary>
+/// An abstraction over the endpoint input validation. (used to be called ModelState validation)
+/// </summary>
+public interface IEndpointParameterValidator
+{
+    /// <summary>
+    /// Validates a given parameter.
+    /// </summary>
+    /// <param name="argument">The parameter/argument to validate</param>
+    /// <returns>A dictionary of code/errors list</returns>
+    public ValueTask<(bool IsValid, IDictionary<string, string[]> Errors)> TryValidateAsync(object argument);
+}
+
+internal class DefaultEndpointParameterValidator : IEndpointParameterValidator
+{
+    public ValueTask<(bool IsValid, IDictionary<string, string[]> Errors)> TryValidateAsync(object argument) =>
+        MiniValidator.TryValidateAsync(argument);
+}
