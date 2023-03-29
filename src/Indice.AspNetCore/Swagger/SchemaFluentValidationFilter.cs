f738b71105e4a4850761503df15f37a3028feb5c
Constantinos Leftheris <c.leftheris@live.com>
2023-03-29 15:22:28 +0300

    configuration

--- src/Indice.AspNetCore/Swagger/SchemaFluentValidationFilter.cs
+++ src/Indice.AspNetCore/Swagger/SchemaFluentValidationFilter.cs
@@ -46,3 +46,67 @@ public class SchemaFluentValidationFilter : ISchemaFilter
                   .Cast<IPropertyValidator>())
                   .Count() > 0;
 }
+
+/// <summary>Filter that decorates request parameters with validators by searching in FluentValidation.</summary>
+public class RequestBodyFluentValidationSwaggerFilter : IRequestBodyFilter
+{
+    private readonly IServiceProvider services;
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="services"></param>
+    public RequestBodyFluentValidationSwaggerFilter(IServiceProvider services) {
+        this.services = services;
+    }
+    /// <inheritdoc/>
+    public void Apply(OpenApiRequestBody requestBody, RequestBodyFilterContext context) {
+        Type validatorType = typeof(IValidator<>).MakeGenericType(context.BodyParameterDescription.Type);
+        IValidator? validator = services.GetService(validatorType) as IValidator;
+        if (validator is not null) {
+            OpenApiSchema schema = context.SchemaRepository.Schemas[context.BodyParameterDescription.Type.FullName];
+            IValidatorDescriptor descriptor = validator.CreateDescriptor();
+            ILookup<string, (IPropertyValidator Validator, IRuleComponent Options)> validationRules = descriptor.GetMembersWithValidators();
+            foreach (IGrouping<string, (IPropertyValidator Validator, IRuleComponent Options)> validationRule in validationRules) {
+                string property = validationRule.Key[..1].ToLower() + validationRule.Key[1..];
+                foreach (IPropertyValidator propertyValidator in validationRule.Select(x => x.Validator)) {
+                    switch (propertyValidator) {
+                        case INotEmptyValidator:
+                            schema.Properties[property].Nullable = false;
+                            break;
+                        case IMinimumLengthValidator minLengthValidator:
+                            schema.Properties[property].MinLength = minLengthValidator.Min;
+                            break;
+                        case IMaximumLengthValidator maxLengthValidator:
+                            schema.Properties[property].MaxLength = maxLengthValidator.Max;
+                            break;
+                        case ILengthValidator lengthValidator:
+                            schema.Properties[property].MinLength = lengthValidator.Min;
+                            schema.Properties[property].MaxLength = lengthValidator.Max;
+                            break;
+                        case IBetweenValidator betweenValidator:
+                            schema.Properties[property].Minimum = Convert.ToDecimal(betweenValidator.From);
+                            schema.Properties[property].Maximum = Convert.ToDecimal(betweenValidator.To);
+                            schema.Properties[property].ExclusiveMinimum = betweenValidator.Name.Contains("exclusive", StringComparison.OrdinalIgnoreCase);
+                            schema.Properties[property].ExclusiveMaximum = betweenValidator.Name.Contains("exclusive", StringComparison.OrdinalIgnoreCase);
+                            break;
+                        case IComparisonValidator comparisonValidator:
+                            if (comparisonValidator.Comparison == Comparison.LessThan) {
+                                schema.Properties[property].Maximum = Convert.ToDecimal(comparisonValidator.ValueToCompare);
+                                schema.Properties[property].ExclusiveMaximum = true;
+                            } else if (comparisonValidator.Comparison == Comparison.LessThanOrEqual) {
+                                schema.Properties[property].Maximum = Convert.ToDecimal(comparisonValidator.ValueToCompare);
+                                schema.Properties[property].ExclusiveMaximum = false;
+                            } else if (comparisonValidator.Comparison == Comparison.GreaterThan) {
+                                schema.Properties[property].Minimum = Convert.ToDecimal(comparisonValidator.ValueToCompare);
+                                schema.Properties[property].ExclusiveMaximum = true;
+                            } else if (comparisonValidator.Comparison == Comparison.GreaterThanOrEqual) {
+                                schema.Properties[property].Minimum = Convert.ToDecimal(comparisonValidator.ValueToCompare);
+                                schema.Properties[property].ExclusiveMaximum = false;
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
