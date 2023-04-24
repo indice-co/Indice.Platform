113b1a2fcca54706ea3d492bab921526bf4c7d95
Constantinos Leftheris <c.leftheris@live.com>
2023-04-24 19:08:56 +0300

    some comments

--- src/Indice.Features.Identity.UI/HttpRequestExtensions.cs
+++ src/Indice.Features.Identity.UI/HttpRequestExtensions.cs
@@ -3,15 +3,24 @@ using UAParser;
 
 namespace Microsoft.AspNetCore.Http;
 
+/// <summary>
+/// Http request extensions
+/// </summary>
 public static class HttpRequestExtensions
 {
+    /// <summary>
+    /// Gets information about the current browser via User-Agent. 
+    /// </summary>
+    /// <param name="request">The http request</param>
+    /// <returns>The browser name (User Agent Family)</returns>
+    /// <remarks>This extension is used solely for informational purposes and logging. It is not considered fool proof</remarks>
     public static string GetBrowserName(this HttpRequest request) {
         var userAgent = request.Headers[HeaderNames.UserAgent];
-        ClientInfo clientInfo = null;
+        ClientInfo? clientInfo = null;
         if (!string.IsNullOrWhiteSpace(userAgent)) {
             var uaParser = Parser.GetDefault();
             clientInfo = uaParser.Parse(userAgent);
         }
-        return clientInfo?.UA?.Family;
+        return clientInfo?.UA?.Family ?? string.Empty;
     }
 }
