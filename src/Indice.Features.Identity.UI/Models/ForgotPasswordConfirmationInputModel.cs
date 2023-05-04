ed4f5973104f903ee9e00054e36498f29e8f982b
Georgios Manoltzas <g.manoltzas@indice.gr>
2023-05-04 19:02:55 +0300

    More pages

---
+++ src/Indice.Features.Identity.UI/Models/ForgotPasswordConfirmationInputModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Indice.Features.Identity.UI.Models;
+
+/// <summary>Contains data required for forgot password confirmation process.</summary>
+public class ForgotPasswordConfirmationInputModel
+{
+    /// <summary>The user's email.</summary>
+    public string? Email { get; set; }
+    /// <summary>The new password.</summary>
+    public string? NewPassword { get; set; }
+    /// <summary>The new password confirmed (optional).</summary>
+    public string? NewPasswordConfirmation { get; set; }
+    /// <summary>The URL to return to.</summary>
+    public string? ReturnUrl { get; set; }
+    /// <summary>The token.</summary>
+    public string? Token { get; set; }
+}
---
+++ src/Indice.Features.Identity.UI/Validators/ForgotPasswordConfirmationInputModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Indice.Features.Identity.UI.Models;
+using Microsoft.Extensions.Localization;
+
+namespace Indice.Features.Identity.UI.Validators;
+
+/// <summary>Validator for <see cref="ForgotPasswordInputModel"/> class.</summary>
+public class ForgotPasswordConfirmationInputModelValidator : AbstractValidator<ForgotPasswordConfirmationInputModel>
+{
+    private readonly IStringLocalizer<ForgotPasswordConfirmationInputModelValidator> _localizer;
+
+    /// <summary>Creates a new instance of <see cref="ForgotPasswordConfirmationInputModelValidator"/> class.</summary>
+    /// <param name="localizer">Represents a service that provides localized strings.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public ForgotPasswordConfirmationInputModelValidator(IStringLocalizer<ForgotPasswordConfirmationInputModelValidator> localizer) {
+        _localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.NewPassword).NotEmpty().WithName(_localizer["New Password"]);
+        RuleFor(x => x.NewPasswordConfirmation).NotEmpty().WithName(_localizer["New Password Confirmation"]);
+        RuleFor(x => x.Token).NotEmpty();
+    }
+}
