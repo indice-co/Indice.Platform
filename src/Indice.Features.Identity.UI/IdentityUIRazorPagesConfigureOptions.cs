b2c5c839bd164108b6c74ac239f699030f3b0631
Georgios Manoltzas <g.manoltzas@indice.gr>
2023-05-09 16:32:21 +0300

    Research for client page overrides

--- src/Indice.Features.Identity.UI/IdentityUIRazorPagesConfigureOptions.cs
+++ src/Indice.Features.Identity.UI/IdentityUIRazorPagesConfigureOptions.cs
@@ -8,5 +8,6 @@ internal class IdentityUIRazorPagesConfigureOptions : IPostConfigureOptions<Razo
     public void PostConfigure(string? name, RazorPagesOptions options) {
         options = options ?? throw new ArgumentNullException(nameof(options));
         options.Conventions.Add(new IdentityUIPageModelConvention());
+        //options.Conventions.Add(new OrderPageRouteModelConvention(int.MaxValue));
     }
 }
---
+++ src/Indice.Features.Identity.UI/OrderPageRouteModelConvention.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Indice.Features.Identity.UI;
+
+internal class OrderPageRouteModelConvention : IPageRouteModelConvention
+{
+    private readonly int _order = 0;
+
+    public OrderPageRouteModelConvention(int order) => _order = order;
+
+    public void Apply(PageRouteModel model) {
+        var libraryPagesRegex = new Regex(@"\/Pages\/[A-Za-z]*.cshtml");
+        if (!libraryPagesRegex.IsMatch(model.RelativePath)) {
+            return;
+        }
+        var selector = model.Selectors.FirstOrDefault();
+        if (selector?.AttributeRouteModel is not null) {
+            selector.AttributeRouteModel.Order = _order;
+        }
+    }
+}
