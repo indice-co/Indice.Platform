19625edbb57ff028a8df97369c3e859992699a28
Constantinos Leftheris <c.leftheris@live.com>
2023-05-17 16:01:52 +0300

    seed options

--- src/Indice.Features.Identity.Core/Data/ExtendedConfigurationDbContextExtensions.cs
+++ src/Indice.Features.Identity.Core/Data/ExtendedConfigurationDbContextExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Indice.Features.Identity.Core.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Indice.Features.Identity.Core.Data;
 
@@ -10,6 +12,9 @@ internal static class ExtendedConfigurationDbContextExtensions
     }
 
     public static void SeedInitialClaimTypes(this ExtendedConfigurationDbContext configurationDbContext) {
+
+        var options = configurationDbContext.GetService<ExtendedConfigurationDbContextSeedOptions>() ?? new ExtendedConfigurationDbContextSeedOptions();
         configurationDbContext.AddRange(InitialClaimTypes.Get());
+        configurationDbContext.AddRange(options.CustomClaims);
     }
 }
---
+++ src/Indice.Features.Identity.Core/Data/ExtendedIdentityDbContextSeedOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Indice.Features.Identity.Core.Data;
+
+/// <summary>
+/// Seed options regarding <see cref="ExtendedIdentityDbContext{TUser, TRole}"/>
+/// </summary>
+public class ExtendedIdentityDbContextSeedOptions<TUser>
+{
+    /// <summary>A list of initial users to be inserted in the database on startup. Works only when environment is 'Development'.</summary>
+    public IEnumerable<TUser> InitialUsers { get; set; } = new List<TUser>();
+}
