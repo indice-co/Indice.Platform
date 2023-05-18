c874f77aee4f9f8de10f90a5f6d8ae734f4ddc22
Georgios Manoltzas <g.manoltzas@indice.gr>
2023-05-18 12:27:30 +0300

    Enhance ClientThemeAttribute to accept more than one client ids

--- src/Indice.Features.Identity.UI/ClientThemeAttribute.cs
+++ src/Indice.Features.Identity.UI/ClientThemeAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace Indice.Features.Identity.UI;
+﻿using System.Collections.ObjectModel;
+
+namespace Indice.Features.Identity.UI;
 
 /// <summary>Used to specify a page override for a specific client id.</summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
@@ -6,11 +8,12 @@ public sealed class ClientThemeAttribute : Attribute
 {
     /// <summary>Creates a new instance of <see cref="ClientThemeAttribute"/>.</summary>
     /// <param name="clientId">The client id.</param>
-    /// <exception cref="ArgumentNullException"></exception>
-    public ClientThemeAttribute(string clientId) {
-        ClientId = clientId ?? throw new ArgumentNullException(nameof(clientId));
+    /// <param name="otherClientIds">Extra client ids to include.</param>
+    public ClientThemeAttribute(string clientId, params string[]? otherClientIds) {
+        otherClientIds ??= Array.Empty<string>();
+        ClientIds = new ReadOnlyCollection<string>(otherClientIds.Concat(new string[] { clientId }).Distinct().ToList());
     }
 
     /// <summary>The client id.</summary>
-    public string ClientId { get; }
+    public ReadOnlyCollection<string> ClientIds { get; }
 }
