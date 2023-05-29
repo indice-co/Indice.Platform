42a270a40f8f329de9e517702e742e744de70795
Constantinos Leftheris <c.leftheris@live.com>
2023-05-29 17:51:47 +0300

    some idieas regarding page selection

--- src/Indice.Features.Identity.UI/IdentityUIRazorPagesConfigureOptions.cs
+++ src/Indice.Features.Identity.UI/IdentityUIRazorPagesConfigureOptions.cs
@@ -8,6 +8,7 @@ internal class IdentityUIRazorPagesConfigureOptions : IPostConfigureOptions<Razo
     public void PostConfigure(string? name, RazorPagesOptions options) {
         options = options ?? throw new ArgumentNullException(nameof(options));
         options.Conventions.Add(new IdentityUIPageModelConvention());
+        //options.Conventions.Add(new IdentityUIPageRouteModelConvention("tailwind"));
         options.Conventions.Add(new OrderPageRouteModelConvention(int.MaxValue));
     }
 }
