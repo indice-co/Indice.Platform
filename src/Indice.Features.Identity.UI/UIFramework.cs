5ac65d68278780c55ca5ccc9c9daf85899518dd8
Constantinos Leftheris <c.leftheris@live.com>
2023-08-25 17:18:41 +0300

    Multiple frameworks support

---
+++ src/Indice.Features.Identity.UI/UIFramework.cs
@@ -0,0 +1,10 @@
+namespace Indice.Features.Identity.UI;
+
+internal enum UIFramework
+{
+    // The default framework for a given release must be 0.
+    // So this needs to be updated in the future if we include more frameworks.
+    Bootstrap4 = 0,
+    Bootstrap5 = 1,
+    Tailwind = 2
+}
