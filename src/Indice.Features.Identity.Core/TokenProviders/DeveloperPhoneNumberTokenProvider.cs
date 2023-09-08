33ce7c8c13f2f7d8bab2d4d1d9b019815c28626f
Georgios Manoltzas <g.manoltzas@indice.gr>
2023-09-08 11:42:30 +0300

    Removed check for environment for developer TOTP, version bump

--- src/Indice.Features.Identity.Core/TokenProviders/DeveloperPhoneNumberTokenProvider.cs
+++ src/Indice.Features.Identity.Core/TokenProviders/DeveloperPhoneNumberTokenProvider.cs
@@ -2,7 +2,6 @@
 using Indice.Features.Identity.Core.Data.Models;
 using Indice.Security;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 namespace Indice.Features.Identity.Core.TokenProviders;
@@ -12,27 +11,19 @@ namespace Indice.Features.Identity.Core.TokenProviders;
 public class DeveloperPhoneNumberTokenProvider<TUser> : ExtendedPhoneNumberTokenProvider<TUser> where TUser : User
 {
     private readonly TotpOptions _options;
-    private readonly IHostEnvironment _environment;
 
     /// <summary>Creates a new instance of <see cref="ExtendedPhoneNumberTokenProvider{TUser}"/>.</summary>
-    /// <param name="options"></param>
-    /// <param name="environment"></param>
-    public DeveloperPhoneNumberTokenProvider(
-        IOptions<TotpOptions> options,
-        IHostEnvironment environment
-    ) : base(Options.Create(new PhoneNumberTokenProviderTotpOptions {
+    /// <param name="options">Configuration used in <see cref="Rfc6238AuthenticationService"/> service.</param>
+    public DeveloperPhoneNumberTokenProvider(IOptions<TotpOptions> options) : base(Options.Create(new PhoneNumberTokenProviderTotpOptions {
         CodeDuration = options.Value.CodeDuration,
         CodeLength = options.Value.CodeLength
     })) {
         _options = options.Value ?? throw new ArgumentNullException(nameof(options));
-        _environment = environment ?? throw new ArgumentNullException(nameof(environment));
     }
-
-    private bool EnableDeveloperTotp => _options.EnableDeveloperTotp && !_environment.IsProduction();
 
     /// <inheritdoc />
     public override async Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<TUser> userManager, TUser user) {
-        if (EnableDeveloperTotp) {
+        if (_options.EnableDeveloperTotp) {
             var userClaims = await userManager.GetClaimsAsync(user);
             var developerTotpClaim = userClaims.FirstOrDefault(claim => claim.Type == BasicClaimTypes.DeveloperTotp);
             var hasDeveloperTotp = developerTotpClaim is not null && await userManager.IsInRoleAsync(user, BasicRoleNames.Developer);
@@ -45,7 +36,7 @@ public class DeveloperPhoneNumberTokenProvider<TUser> : ExtendedPhoneNumberToken
 
     /// <inheritdoc />
     public override async Task<string> GenerateAsync(string purpose, UserManager<TUser> userManager, TUser user) {
-        if (EnableDeveloperTotp) {
+        if (_options.EnableDeveloperTotp) {
             var userClaims = await userManager.GetClaimsAsync(user);
             var developerTotpClaim = userClaims.FirstOrDefault(claim => claim.Type == BasicClaimTypes.DeveloperTotp);
             if (!string.IsNullOrWhiteSpace(developerTotpClaim?.Value)) {
@@ -57,7 +48,7 @@ public class DeveloperPhoneNumberTokenProvider<TUser> : ExtendedPhoneNumberToken
 
     /// <inheritdoc />
     public override async Task<bool> ValidateAsync(string purpose, string token, UserManager<TUser> userManager, TUser user) {
-        if (EnableDeveloperTotp) {
+        if (_options.EnableDeveloperTotp) {
             var userClaims = await userManager.GetClaimsAsync(user);
             var developerTotpClaim = userClaims.FirstOrDefault(claim => claim.Type == BasicClaimTypes.DeveloperTotp);
             if (developerTotpClaim?.Value == token) {
