20bfe1a4f41e9b621a1a60ab546681951f4636ec
Fotis Tsinopoulos <f.tsinopoulos@indice.gr>
2023-09-14 13:14:27 +0300

    Integration Tests for Messages

---
+++ test/Indice.Features.Messages.Tests/Mocks/MockContactResolver.cs
@@ -0,0 +1,26 @@
+﻿using Indice.Features.Messages.Core.Models;
+using Indice.Features.Messages.Core.Services.Abstractions;
+using Indice.Types;
+
+namespace Indice.Features.Messages.Tests.Mocks;
+internal class MockContactResolver : IContactResolver
+{
+    private List<Contact> _contacts = new() { 
+        new Contact() {
+            RecipientId = "6c9fa6dd-ede4-486b-bf91-6de18542da4a",
+            FirstName = "Indice",
+            LastName = "User",
+            FullName = "Indice User",
+            Email = "company@indice.gr",
+            PhoneNumber = "1234567890",
+            UpdatedAt = DateTime.UtcNow.AddMinutes(-10)
+        }
+    };
+    public Task<ResultSet<Contact>> Find(ListOptions options) {
+        return Task.FromResult(_contacts.ToResultSet());
+    }
+
+    public Task<Contact> Resolve(string recipientId) {
+        return Task.FromResult(_contacts.FirstOrDefault(i => i.RecipientId == recipientId));
+    }
+}
---
+++ test/Indice.Features.Messages.Tests/Security/DummyAuthExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Indice.Features.Messages.Tests.Security;
+public static class DummyAuthExtensions
+{
+    public static AuthenticationBuilder AddDummy(this AuthenticationBuilder builder, Func<ClaimsPrincipal> CreateCrincipal)
+        => builder.AddScheme<DummyAuthOptions, DummyAuthHandler>(DummyAuthDefaults.AuthenticationScheme, "Dummy Auth", o => o.CreatePrincipal = CreateCrincipal);
+}
