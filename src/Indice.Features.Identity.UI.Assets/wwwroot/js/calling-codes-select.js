3a7b1e9fb825ee6975c4128da6aaae4cfee2f3fe
Panagiotis Athanasiou <p.athanasiou@indice.gr>
2023-09-19 13:55:38 +0300

    add calling-codes-select.js

---
+++ src/Indice.Features.Identity.UI.Assets/wwwroot/js/calling-codes-select.js
@@ -0,0 +1,27 @@
+﻿!function () {
+    const id = document.currentScript.getAttribute('data-id');
+    const callingCodes = document.getElementById(id);
+
+    if (callingCodes == undefined) return;
+
+    function focus() {
+        [].forEach.call(this.options, (option) => {
+            option.textContent = option.getAttribute('data-text');
+        });
+    }
+    function blur() {
+        [].forEach.call(this.options, (option) => {
+            if (option.selected) {
+                option.textContent = '+' + option.getAttribute('value');
+            }
+            else {
+                option.textContent = option.getAttribute('data-text');
+            }
+        });
+    }
+    callingCodes.addEventListener('focus', focus);
+    callingCodes.addEventListener('blur', blur);
+    callingCodes.addEventListener('change', blur);
+
+    blur.call(callingCodes);
+}();
