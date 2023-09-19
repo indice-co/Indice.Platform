3b16018f6b11bf1293b0b5f409926d8491033c86
Georgios Manoltzas <g.manoltzas@indice.gr>
2023-09-19 15:20:39 +0300

    Minor fix on GetUser endpoint, generate admin UI client

--- src/Indice.Features.Identity.Server/Manager/UserHandlers.cs
+++ src/Indice.Features.Identity.Server/Manager/UserHandlers.cs
@@ -84,7 +84,7 @@ internal static class UserHandlers
                 Email = user.Email,
                 EmailConfirmed = user.EmailConfirmed,
                 LockoutEnabled = user.LockoutEnabled,
-                LockoutEnd = user.LockoutEnd,
+                LockoutEnd = user.LockoutEnabled ? (user.LockoutEnd > DateTimeOffset.UtcNow ? user.LockoutEnd : null) : null,
                 PhoneNumber = user.PhoneNumber,
                 PhoneNumberConfirmed = user.PhoneNumberConfirmed,
                 TwoFactorEnabled = user.TwoFactorEnabled,
