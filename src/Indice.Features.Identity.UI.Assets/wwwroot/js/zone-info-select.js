46d83b3a78564e18d88b21de15855a828c8d21d1
Panagiotis Athanasiou <p.athanasiou@indice.gr>
2023-09-27 16:32:19 +0300

    Update tailwind pages

--- src/Indice.Features.Identity.UI.Assets/wwwroot/js/zone-info-select.js
+++ src/Indice.Features.Identity.UI.Assets/wwwroot/js/zone-info-select.js
@@ -1,14 +1,13 @@
 !function () {
     const id = document.currentScript.getAttribute('data-id');
     const input = document.getElementById(id);
-    const list = document.getElementById(id + "_list");
 
-    if (input == undefined || list == undefined) return;
+    if (input == undefined) return;
 
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const first = [].find.call(list.options, (option => timezone === option.value));
+    const first = [].find.call(input.options, (option => timezone === option.value));
 
     if (first === undefined) return;
 
-    input.value = first.value;
+    first.selected = true;
 }();
