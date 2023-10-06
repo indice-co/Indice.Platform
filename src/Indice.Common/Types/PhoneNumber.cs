f6943561a56f9e9ca495e7ac15ee3226ba2283ff
Constantinos Leftheris <c.leftheris@live.com>
2023-10-06 15:17:45 +0300

    added PhoneNumber struct

---
+++ src/Indice.Common/Types/PhoneNumber.cs
@@ -0,0 +1,94 @@
+﻿#nullable enable
+using System.Text.RegularExpressions;
+
+namespace Indice.Globalization;
+
+/// <summary>Encapsulates an International number format</summary>
+public partial struct PhoneNumber
+{
+    private const string RegexPatternString = @"(\+(?<CallingCode>\d+(-\d+)?) (?<Number>[\d() -]{5,15}))|(?<GreekNumber>69\d{8})";
+    private const char PlusSign = '+';
+    private static readonly Regex _Pattern = PhoneNumberFormat();
+
+    /// <summary>The calling code without the plus sign.</summary>
+    public string CallingCode { get; }
+    /// <summary>Two letter ISO country code.</summary>
+    public string TwoLetterCountryCode { get; }
+    /// <summary>Area code and Subscriber number</summary>
+    public string Number { get; }
+
+    /// <summary>Create a format to parse and format a number</summary>
+    public PhoneNumber(string callingCode, string twoLetterCountryCode, string number) {
+        if (callingCode.Contains(PlusSign)) {
+            throw new ArgumentException("Cannot contain the plus sign", nameof(callingCode));
+        }
+        if (twoLetterCountryCode.Length != 2) {
+            throw new ArgumentException("should be exactly 2 characters long", nameof(twoLetterCountryCode));
+        }
+        CallingCode = callingCode;
+        TwoLetterCountryCode = twoLetterCountryCode;
+        Number = number;
+    }
+
+#if NET7_0_OR_GREATER
+    [GeneratedRegex(RegexPatternString)]
+    private static partial Regex PhoneNumberFormat();
+#else 
+    private static Regex PhoneNumberFormat() => new(RegexPatternString, RegexOptions.Compiled);
+#endif
+
+    /// <inheritdoc/>
+    public override string ToString() => $"{PlusSign}{CallingCode} {Number}";
+
+    /// <summary>
+    /// Parses the given phone number using.
+    /// </summary>
+    /// <param name="phoneNumber"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    /// <exception cref="FormatException"></exception>
+    public static PhoneNumber Parse(string phoneNumber) {
+        if (phoneNumber is null) {
+            throw new ArgumentNullException(nameof(phoneNumber));
+        }
+        var match = _Pattern.Match(phoneNumber);
+        if (!match.Success) {
+            throw new FormatException("The phoneNumber supplied was not in the correct format. Expected ie +30 6900000000");
+        }
+        // check for greek mobile phone
+        if (match.Groups["GreekNumber"].Success) {
+            return new PhoneNumber("30", "GR", match.Groups["GreekNumber"].Value);
+        }
+        var callingCode = match.Groups["CallingCode"].Value;
+        var number = match.Groups["Number"].Value;
+        if (!CountryInfo.TryGetCountryByCallingCode(callingCode, out var country)) {
+            throw new FormatException($"The phoneNumber supplied has a calling code that is not corralate to a known country. Code '{callingCode}'");
+        }
+        return new PhoneNumber(callingCode, country.TwoLetterCode, number.Replace("-", "")
+                                                                         .Replace("(", "")
+                                                                         .Replace(")", "")
+                                                                         .Replace(" ", ""));
+    }
+
+    /// <summary>Tries to convert the specified <paramref name="textInput"/> to a <see cref="PhoneNumber"/>.</summary>
+    /// <param name="textInput">The phone number string to convert.</param>
+    /// <param name="phoneNumber">The converted <see cref="PhoneNumber"/>.</param>
+    /// <returns>True if conversion is successful, otherwise false.</returns>
+    public static bool TryParse(string textInput, out PhoneNumber phoneNumber) {
+        phoneNumber = default;
+        try {
+            phoneNumber = Parse(textInput);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    /// <summary>Implicit cast from <see cref="PhoneNumber"/> to <seealso cref="string"/>.</summary>
+    /// <param name="value">The value to convert.</param>
+    public static implicit operator string(PhoneNumber value) => value.ToString();
+    /// <summary>Explicit cast from <see cref="string"/> to <seealso cref="PhoneNumber"/></summary>
+    /// <param name="value">The value to convert.</param>
+    public static explicit operator PhoneNumber(string value) => Parse(value);
+}
+#nullable disable
---
+++ test/Indice.Common.Tests/PhoneNumberTests.cs
@@ -0,0 +1,24 @@
+﻿using Indice.Globalization;
+using Xunit;
+
+namespace Indice.Common.Tests;
+
+public class PhoneNumberTests
+{
+    public PhoneNumberTests() {
+
+    }
+
+    [Trait("Tag", "PhoneNumber")]
+    [Theory]
+    [InlineData("+1 (0115) 123 4567")]
+    [InlineData("+30 6972233445")]
+    [InlineData("+30 (697) 2233445")]
+    [InlineData("+30 210-6985955")]
+    [InlineData("+44 (0141) 345 9822")]
+    [InlineData("+1 800-444-4444")]
+    public void ByTwoLetterISOCodeValid_Test(string number) {
+        var phone = PhoneNumber.Parse(number);
+        Assert.True(true);
+    }
+}
