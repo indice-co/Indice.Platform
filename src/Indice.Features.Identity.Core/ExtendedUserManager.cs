54ee155be2bb8e98e1c5c552f5cd72cfcc7d852f
eskour <125371831+eskour@users.noreply.github.com>
2023-10-13 14:36:10 +0300

    Enrich UserNameChangedEvent with previous user state (#324)
    * Enrich UserNameChangedEvent with previous UserName
    
    * Track PreviousUserState

--- src/Indice.Features.Identity.Core/ExtendedUserManager.cs
+++ src/Indice.Features.Identity.Core/ExtendedUserManager.cs
@@ -148,9 +148,10 @@ public class ExtendedUserManager<TUser> : UserManager<TUser> where TUser : User
 
     /// <inheritdoc />
     public async override Task<IdentityResult> SetUserNameAsync(TUser user, string userName) {
+        var previousUserState = user;
         var result = await base.SetUserNameAsync(user, EmailAsUserName ? user.Email : userName);
         if (result.Succeeded) {
-            await _eventService.Publish(new UserNameChangedEvent(user));
+            await _eventService.Publish(new UserNameChangedEvent(user, previousUserState));
         }
         return result;
     }
