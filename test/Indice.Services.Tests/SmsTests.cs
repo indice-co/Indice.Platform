1acc89b0a8e9a1b4ea0a1045c04be714cbdd5341
Tzortzis Kondylis <t.kondylis@indice.gr>
2024-01-04 18:47:38 +0200

    Mstat working implementation for Viber-Sms service

--- test/Indice.Services.Tests/SmsTests.cs
+++ test/Indice.Services.Tests/SmsTests.cs
@@ -1,6 +1,9 @@
 using Indice.Services.Yuboto;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
+using Moq;
 using Xunit;
 
 namespace Indice.Services.Tests;
@@ -12,14 +15,16 @@ public class SmsTests
     public async Task TestYubotoOmniSms(string apiKey, string phoneNumber, string sender, string senderName, string subject, string body) {
         var exception = default(Exception);
         try {
+            var optionsSnapshotMock = new Mock<IOptionsSnapshot<SmsServiceSettings>>();
+            optionsSnapshotMock.Setup(x => x.Value).Returns(new SmsServiceSettings {
+                ApiKey = apiKey,
+                Sender = sender,
+                SenderName = senderName,
+                TestMode = true
+            });
             var service = new SmsYubotoOmniService(
                 new HttpClient(),
-                Options.Create(new SmsServiceSettings {
-                    ApiKey = apiKey,
-                    Sender = sender,
-                    SenderName = senderName,
-                    TestMode = true
-                }) as IOptionsSnapshot<SmsServiceSettings>,
+                optionsSnapshotMock.Object,
                 new NullLogger<SmsYubotoOmniService>()
          );
             await service.SendAsync(phoneNumber, subject, body);
@@ -32,19 +37,57 @@ public class SmsTests
     [Theory(Skip = "Sensitive Data")]
     [InlineData("", "", "", "Indice", "Indice", "Test Subject", "Test Body")]
     public async Task TestApifonSms(string apiKey, string token, string phoneNumber, string sender, string senderName, string subject, string body) {
+        var inMemorySettings = new Dictionary<string, string> {
+            ["Sms:ApiKey"] = apiKey,
+            ["Sms:Token"] = token,
+            ["Sms:Sender"] = sender,
+            ["Sms:SenderName"] = senderName,
+            ["Sms:TestMode"] = true.ToString()
+        };
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+        var collection = new ServiceCollection()
+            .AddSingleton(configuration)
+            .AddOptions()
+            .Configure<SmsServiceApifonSettings>(configuration.GetSection(SmsServiceApifonSettings.Name))
+            .AddSmsServiceApifon(configuration);
+        
+
+        var serviceProvider = collection.BuildServiceProvider();
+        var excepion = default(Exception);
+        
+        try {
+            var service = serviceProvider.GetRequiredService<ISmsService>();
+            await service.SendAsync(phoneNumber, subject, body);
+        } catch (Exception smsServiceException) {
+            excepion = smsServiceException;
+        }
+        Assert.Null(excepion);
+    }
+
+    [Theory(Skip = "Sensitive Data")]
+    [InlineData("", "", "", "Test Subject", "Test Body", "Test")]
+    public async Task TestMstatSms(string apiToken, string phoneNumber, string sender, string subject, string body, string senderName) {
+
+        var inMemorySettings = new Dictionary<string, string> {
+            ["Sms:ApiKey"] = apiToken,
+            ["Sms:Sender"] = sender,
+            ["Sms:SenderName"] = senderName
+        };
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+            .Build();
+        var collection = new ServiceCollection()
+            .AddSingleton(configuration)
+            .AddOptions()
+            .Configure<SmsServiceMstatSettings>(configuration.GetSection(SmsServiceMstatSettings.Name))
+            .AddSmsServiceMstat(configuration);
+
+        var serviceProvider = collection.BuildServiceProvider();
         var excepion = default(Exception);
         try {
-            var service = new SmsServiceApifon(
-                new HttpClient { BaseAddress = new Uri("https://ars.apifon.com/services/api/v1/sms/") },
-                Options.Create(new SmsServiceApifonSettings {
-                    ApiKey = apiKey,
-                    Token = token,
-                    Sender = sender,
-                    SenderName = senderName,
-                    TestMode = true
-                }) as IOptionsSnapshot<SmsServiceApifonSettings>,
-                new NullLogger<SmsServiceApifon>()
-            );
+            var service = serviceProvider.GetRequiredService<ISmsService>();
             await service.SendAsync(phoneNumber, subject, body);
         } catch (Exception smsServiceException) {
             excepion = smsServiceException;
