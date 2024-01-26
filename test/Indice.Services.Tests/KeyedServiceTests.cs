3bb10fae9ed478ebcdf765bfb534eb0d021492b1
Constantinos Leftheris <c.leftheris@live.com>
2024-01-26 15:31:21 +0200

    more keyed service tests

--- test/Indice.Services.Tests/KeyedServiceTests.cs
+++ test/Indice.Services.Tests/KeyedServiceTests.cs
@@ -17,18 +17,26 @@ public class KeyedServiceTests
         var collection = new ServiceCollection()
             .AddSingleton(configuration)
             .AddOptions()
-            .AddKeyedScoped<IFileService, FileServiceInMemory>("serviceA")
-            .AddKeyedScoped<IFileService, FileServiceAzureStorage>("serviceB", (sp, key) => new FileServiceAzureStorage(key.ToString(), key.ToString()))
-            .AddKeyedScoped<IFileService, FileServiceAzureStorage>("serviceC", (sp, key) => new FileServiceAzureStorage(key.ToString(), key.ToString()));
+            .AddTransient<IFileService>(sp => new FileServiceAzureStorage("serviceDefault", "serviceDefault"))
+            .AddKeyedTransient<IFileService, FileServiceInMemory>("serviceA")
+            .AddKeyedTransient<IFileService, FileServiceAzureStorage>("serviceB", (sp, key) => new FileServiceAzureStorage(key.ToString(), key.ToString()))
+            .AddKeyedTransient<IFileService, FileServiceAzureStorage>("serviceC", (sp, key) => new FileServiceAzureStorage(key.ToString(), key.ToString()));
         var serviceProvider = collection.BuildServiceProvider();
 
+        var serviceDefault = serviceProvider.GetRequiredService<IFileService>();
         var serviceB = serviceProvider.GetKeyedService<IFileService>("serviceB");
         var serviceC = serviceProvider.GetKeyedService<IFileService>("serviceC");
 
         Assert.IsType<FileServiceAzureStorage>(serviceB);
         Assert.IsType<FileServiceAzureStorage>(serviceC);
         var connectionString = typeof(Indice.Services.FileServiceAzureStorage).GetField("_connectionString", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+#if NET8_0_OR_GREATER
         Assert.Equal("serviceB", connectionString.GetValue(serviceB));
         Assert.Equal("serviceC", connectionString.GetValue(serviceC));
+#else
+        Assert.Equal("serviceC", connectionString.GetValue(serviceB)); // <-- Should be "serviceB"!!!!!. Although is expected behavior not to work some day we should make it be like dotnet 8 or abandon previous versions.
+        Assert.Equal("serviceC", connectionString.GetValue(serviceC));
+#endif
+        Assert.Equal("serviceDefault", connectionString.GetValue(serviceDefault));
     }
 }
