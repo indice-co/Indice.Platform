338fbd39ba606cc607a141aaa4b1780335722d94
Constantinos Leftheris <c.leftheris@live.com>
2024-01-31 13:01:38 +0200

    enrich signin log failure with client id and name from web ui

--- src/Indice.Features.Identity.Core/Events/ExtendedUserLoginFailureEvent.cs
+++ src/Indice.Features.Identity.Core/Events/ExtendedUserLoginFailureEvent.cs
@@ -9,14 +9,19 @@ namespace Indice.Features.Identity.Core.Events;
 /// <param name="interactive">Specifies if login was interactive</param>
 /// <param name="clientId">The client id</param>
 /// <param name="subjectId">The subject id.</param>
+/// <param name="clientName">The client Name</param>
 public class ExtendedUserLoginFailureEvent(
     string username,
     string error,
     bool interactive = true,
     string clientId = null,
-    string subjectId = null
+    string subjectId = null,
+    string clientName = null
 ) : UserLoginFailureEvent(username, error, interactive, clientId)
 {
     /// <summary>The subject id.</summary>
     public string SubjectId { get; } = subjectId;
+
+    /// <summary>The client name.</summary>
+    public string ClientName { get; } = clientName;
 }
