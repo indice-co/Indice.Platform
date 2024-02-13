867847b55ad0b38ae74ee9d6332eaca4881d8dd7
Aris Triantafyllou <a.triantafyllou@indice.gr>
2024-02-13 16:45:05 +0200

    Cases UI improvements

--- src/Indice.Features.Cases.App/src/app/shared/ajsf/json-schema-frameworks/tailwind-framework/label-only/label-only.component.ts
+++ src/Indice.Features.Cases.App/src/app/shared/ajsf/json-schema-frameworks/tailwind-framework/label-only/label-only.component.ts
@@ -1,6 +1,20 @@
 import { JsonSchemaFormService } from '@ajsf-extended/core';
 import { Component, Input, OnInit } from '@angular/core';
 
+/**
+ * Cases UI widget component for usage in Layout.json.
+ * Displays form input data as a simple label.
+ * 
+ * @example
+ * ```
+  {
+    "title": "Πεδίο",
+    "labelHtmlClass": "font-bold",
+    "type": "label-only",
+    "flex": "1 1 50px"
+  },
+ * ```
+ */
 @Component({
   selector: 'app-label-only',
   templateUrl: './label-only.component.html',
@@ -25,5 +39,50 @@ export class LabelOnlyComponent implements OnInit {
   ngOnInit() {
     this.options = this.layoutNode.options || {};
     this.jsf.initializeControl(this);
+    this.trySetEnumType();
+    this.trySetCurrencyType();
+  }
+
+  /**
+ * Configures label to be in currency format if passed as extraType option
+ * 
+ * @example
+ * ```
+  {
+    "key": "amount",
+    "labelHtmlClass": "font-bold",
+    "type": "label-only",
+    "extraType": "currency",
+    "flex": "1 1 280px",
+    "title": "Αμοθντ"
+  }
+ * ```
+ */
+  private trySetCurrencyType() {
+    if (this.layoutNode.options.extraType == 'currency') {
+      this.controlValue = parseFloat(this.formControl.value).toLocaleString('el');
+      this.jsf.updateValue(this, this.controlValue);
+    }
+  }
+
+  /** Attempts to set enumeration to corresponding values if exist*/
+  private trySetEnumType() {
+    if (this.hasEnumType()) {
+      (this.options.enum as string[]).forEach((enumValue, index) => {
+        if (this.controlValue === enumValue) {
+          this.controlValue = this.options.enumNames[index];
+          this.jsf.updateValue(this, this.controlValue);
+        }
+      });
+    }
+  }
+
+/** Checks whether layoutNode contains enumeration values */
+  private hasEnumType(): boolean {
+    return this.options.enum &&
+      this.options.enumNames &&
+      Array.isArray(this.options.enum) &&
+      Array.isArray(this.options.enumNames) &&
+      this.options.enum.length === this.options.enumNames.length;
   }
 }
