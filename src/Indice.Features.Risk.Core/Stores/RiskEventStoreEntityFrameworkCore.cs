bdad5cd21ef219899037244ae38197c569ee07cd
Efthymios Skouras <e.skouras@indice.gr>
2024-02-21 14:35:59 +0200

    Make date range comparison more flexible

--- src/Indice.Features.Risk.Core/Stores/RiskEventStoreEntityFrameworkCore.cs
+++ src/Indice.Features.Risk.Core/Stores/RiskEventStoreEntityFrameworkCore.cs
@@ -37,17 +37,21 @@ internal class RiskEventStoreEntityFrameworkCore : IRiskEventStore
     }
 
     private IQueryable<RiskEvent> ApplyFilter(IQueryable<RiskEvent> query, FilterClause[] filter) {
+        var dateFromClause = filter.FirstOrDefault(c => c.Member.ToLower() == "from");
+        var dateToClause = filter.FirstOrDefault(c => c.Member.ToLower() == "to");
+
+        if (DateTimeOffset.TryParse(dateFromClause.Value, out var dateFrom) && DateTimeOffset.TryParse(dateToClause.Value, out var dateTo)) {
+            if (dateFrom == dateTo) {
+                dateTo = dateTo.Date.AddDays(1).AddTicks(-1);
+            }
+
+            query = query.Where(c => c.CreatedAt >= dateFrom);
+            query = query.Where(c => c.CreatedAt <= dateTo);
+        }
+
         foreach (var clause in filter) {
             if (string.IsNullOrWhiteSpace(clause.Member)) {
                 continue;
-            }
-
-            if (clause.Member.ToLower() == "from" && DateTimeOffset.TryParse(clause.Value, out var dateFrom)) {
-                query = query.Where(c => c.CreatedAt >= dateFrom.Date);
-            }
-
-            if (clause.Member.ToLower() == "to" && DateTimeOffset.TryParse(clause.Value, out var dateTo)) {
-                query = query.Where(c => c.CreatedAt <= dateTo.Date.AddDays(1));
             }
 
             if (clause.Member.Equals(nameof(RiskEvent.SubjectId), StringComparison.OrdinalIgnoreCase)) {
