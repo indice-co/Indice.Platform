9ed8f4eb873210097e01ec5fb9487eb70eef37f7
Efthymios Skouras <e.skouras@indice.gr>
2024-02-27 16:23:58 +0200

    remove useless dependencies

--- src/Indice.Extensions.Configuration.Database/DatabaseConfigurationExtensions.cs
+++ src/Indice.Extensions.Configuration.Database/DatabaseConfigurationExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Indice.Extensions.Configuration.Database;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,26 +9,6 @@ namespace Microsoft.AspNetCore.Hosting;
 /// <summary>Extension methods for registering <see cref="EntityConfigurationProvider{T}"/> with <see cref="IConfigurationBuilder"/>.</summary>
 public static class DatabaseConfigurationExtensions
 {
-    /// <summary>Registers and configures the <see cref="EntityConfigurationProvider{T}"/> using some default values.</summary>
-    /// <typeparam name="TContext"></typeparam>
-    /// <param name="webHostBuilder"></param>
-    /// <param name="configureAction"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentException"></exception>
-    public static WebApplicationBuilder AddDatabaseSettings<TContext>(this WebApplicationBuilder webHostBuilder, Action<EntityConfigurationOptions, IConfiguration> configureAction) where TContext : DbContext, IAppSettingsDbContext {
-        var configurationBuilder = new ConfigurationBuilder();
-        var options = new EntityConfigurationOptions();
-        configureAction?.Invoke(options, configurationBuilder.Build());
-        var result = options.Validate();
-        if (!result.Succedded) {
-            throw new ArgumentException(result.Error);
-        }
-        configurationBuilder.Add(new EntityConfigurationSource<TContext>(options));
-        webHostBuilder.Configuration.AddConfiguration(configurationBuilder.Build());
-        webHostBuilder.Services.AddTransient<IAppSettingsDbContext, TContext>();
-        return webHostBuilder;
-    }
-
     /// <summary>Registers and configures the <see cref="EntityConfigurationProvider{T}"/> using some default values.</summary>
     /// <param name="webHostBuilder">A builder for <see cref="IWebHost"/>.</param>
     /// <param name="configureAction">The <see cref="EntityConfigurationOptions"/> to use.</param>
