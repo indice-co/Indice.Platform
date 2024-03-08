65c8582f695221ca7e41485c7eee15c078e44fd0
Aris Triantafyllou <a.triantafyllou@indice.gr>
2024-03-08 16:49:02 +0200

    Extend GetList for RiskEvents

--- src/Indice.Features.Risk.Core/Abstractions/IRiskEventStore.cs
+++ src/Indice.Features.Risk.Core/Abstractions/IRiskEventStore.cs
@@ -12,29 +12,21 @@ public interface IRiskEventStore
     Task CreateAsync(RiskEvent @event);
 
     /// <summary>Returns the risk events associated with a given subject ID</summary>
-    /// <param name="subjectId">The subject id.</param>
-    /// <param name="types">The event types.</param>
-    Task<IEnumerable<RiskEvent>> GetList(string subjectId, string[]? types = null);
+    /// <param name="subjectId">The identifier of the subject (user) for which risk events are retrieved.</param>
+    /// <param name="types">Optional. The event types to filter out specific risk events.</param>
+    /// <param name="startDate">Optional. The start date from which risk events are considered.</param>
+    /// <param name="endDate">Optional. The end date until which risk events are considered. If not provided, the current date is used.</param>
+    /// <param name="filters">Optional. Additional filters to apply to the risk events.</param>
+    Task<IEnumerable<RiskEvent>> GetList(
+        string subjectId, 
+        string[]? types = null,
+        DateTime? startDate = null,
+        DateTime? endDate = null,
+        List<FilterClause>? filters = null);
 
     /// <summary>
     /// Fetches risk events from the store
     /// </summary>
     /// <param name="options"></param>
     Task<ResultSet<RiskEvent>> GetList(ListOptions<AdminRiskFilterRequest> options);
-
-    /// <summary>
-    /// Retrieves historical risk events for a specific SubjectId (User Id) starting from a given date.
-    /// </summary>
-    /// <param name="subjectId">The identifier of the subject (user) for which historical risk events are retrieved.</param>
-    /// <param name="startDate">The start date from which historical risk events are considered.</param>
-    /// <param name="endDate">Optional. The end date until which historical risk events are considered. If not provided, the current date is used.</param>
-    /// <param name="filters">Optional. Additional filters to apply to the historical risk events.</param>
-    /// <param name="type">Optional. The type to filter out specific historical risk events.</param>
-    /// <returns>An enumerable collection of <see cref="RiskEvent"/> objects representing historical risk events.</returns>
-    Task<IEnumerable<RiskEvent>> GetHistoricalRiskEvents(
-        string subjectId,
-        DateTime startDate,
-        DateTime? endDate = null,
-        List<FilterClause>? filters = null,
-        string? type = null);
 }
--- src/Indice.Features.Risk.Core/Stores/RiskEventStoreNoOp.cs
+++ src/Indice.Features.Risk.Core/Stores/RiskEventStoreNoOp.cs
@@ -9,14 +9,12 @@ internal class RiskEventStoreNoOp : IRiskEventStore
 {
     public Task CreateAsync(RiskEvent @event) => Task.CompletedTask;
 
-    public Task<IEnumerable<RiskEvent>> GetList(string subjectId, string[]? types) => Task.FromResult(Enumerable.Empty<RiskEvent>());
+    public Task<IEnumerable<RiskEvent>> GetList(
+        string subjectId, 
+        string[]? types,
+        DateTime? startDate,
+        DateTime? endDate,
+        List<FilterClause>? filters) => Task.FromResult(Enumerable.Empty<RiskEvent>());
 
     public Task<ResultSet<RiskEvent>> GetList(ListOptions<AdminRiskFilterRequest> options) => Task.FromResult(new ResultSet<RiskEvent>());
-
-    public Task<IEnumerable<RiskEvent>> GetHistoricalRiskEvents(
-        string subjectId, 
-        DateTime startDate, 
-        DateTime? endDate = null, 
-        List<FilterClause>? filters = null, 
-        string? type = null) => Task.FromResult(Enumerable.Empty<RiskEvent>());
 }
