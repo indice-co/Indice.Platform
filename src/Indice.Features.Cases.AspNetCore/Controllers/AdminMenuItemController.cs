c87fa0ddb3752f494e0b2b8bebe4f5508ea53efd
Konstantinos Argyropoulos <k.argyropoulos@indice.gr>
2024-04-29 17:48:05 +0300

    Removed get case type by id endpoint

--- src/Indice.Features.Cases.AspNetCore/Controllers/AdminMenuItemController.cs
+++ src/Indice.Features.Cases.AspNetCore/Controllers/AdminMenuItemController.cs
@@ -23,7 +23,6 @@ internal class AdminMenuItemController : ControllerBase
 {
     private readonly ICaseTypeMenuService _caseTypeMenuService;
 
-
     public AdminMenuItemController(ICaseTypeMenuService caseTypeMenuService) {
         _caseTypeMenuService = caseTypeMenuService ?? throw new ArgumentNullException(nameof(caseTypeMenuService));
     }
@@ -33,16 +32,5 @@ internal class AdminMenuItemController : ControllerBase
     public async Task<IActionResult> GetMenuItems([FromQuery] ListOptions options) {
         var menuItems = await _caseTypeMenuService.GetMenuItems(options);
         return Ok(menuItems);
-    }
-
-    /// <summary>Gets the list of all cases using the provided <see cref="ListOptions"/>.</summary>
-    /// <param name="options">List params used to navigate through collections. Contains parameters such as sort, search, page number and page size.</param>
-    /// <response code="200">OK</response>
-    [HttpGet("{caseTypeId:guid}")]
-    [Produces(MediaTypeNames.Application.Json)]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResultSet<CasePartial>))]
-    public async Task<IActionResult> GetCasesByCaseTypeId([FromQuery] ListOptions<GetCasesListFilter> options, Guid caseTypeId) {
-        var cases = await _caseTypeMenuService.GetCasesByCaseTypeId(User, options, caseTypeId);
-        return Ok(cases);
     }
 }
--- src/Indice.Features.Cases.AspNetCore/Interfaces/ICaseTypeMenuService.cs
+++ src/Indice.Features.Cases.AspNetCore/Interfaces/ICaseTypeMenuService.cs
@@ -20,11 +20,4 @@ public interface ICaseTypeMenuService
     /// </summary>
     /// <returns>Result set of <see cref="CaseTypeMenu"/></returns>
     Task<ResultSet<CaseTypeMenu>> GetMenuItems(ListOptions options);
-
-    /// <summary>Get a list of cases as defined by <see cref="GetCasesListFilter"/> and the role of the user.</summary>
-    /// <param name="user">The user that initiated the request</param>
-    /// <param name="options">The case list filters.</param>
-    /// <param name="caseTypeId">The case type id.</param>
-    /// <returns></returns>
-    Task<ResultSet<CasePartial>> GetCasesByCaseTypeId(ClaimsPrincipal user, ListOptions<GetCasesListFilter> options, Guid caseTypeId);
 }
