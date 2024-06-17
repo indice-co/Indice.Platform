c38b016fd1125fda17c4db611b4d1614c6513b04
Constantinos Leftheris <c.leftheris@live.com>
2024-06-17 19:24:00 +0300

    greeklish elot

---
+++ test/Indice.Features.Messages.Tests/SlugifyPathTests.cs
@@ -0,0 +1,17 @@
+﻿using Indice.Extensions;
+using Indice.Text;
+using Xunit;
+
+namespace Indice.Features.Messages.Tests;
+public class SlugifyPathTests
+{
+    [InlineData("Λάχανο Ελληνικό Ποιότητα Α΄ Τιμή Κιλού", "lachano-elliniko-poiotita-a-timi-kilou")]
+    [InlineData("Κρεμμύδια Ξερά Χοντρά Τιμή Κιλού", "kremmydia-xera-chontra-timi-kilou")]
+    [Theory]
+    public void GreekOrUnicodeMustConvertToMeaningfull_ASCII_Test(string input, string expected) {
+        
+        var output = Greeklish.Translate(input).Unidecode().ToKebabCase();
+
+        Assert.Equal(expected, output);
+    }
+}
