9c831808031a33df2723b08ce828746c3c9a0a8c
Konstantinos Argyropoulos <k.argyropoulos@indice.gr>
2024-06-20 10:27:05 +0300

    Case type filter is now readonly

--- src/Indice.Features.Cases.App/src/app/features/cases/case-type-specific-cases/case-type-specific-cases.component.ts
+++ src/Indice.Features.Cases.App/src/app/features/cases/case-type-specific-cases/case-type-specific-cases.component.ts
@@ -58,7 +58,8 @@ export class CaseTypeSpecificCasesComponent extends GeneralCasesComponent implem
       name: 'ΤΥΠΟΣ ΥΠΟΘΕΣΗΣ',
       dataType: 'array',
       options: [],
-      multiTerm: true
+      multiTerm: true,
+      readonly: true
     }
     const code = this.getFilterCacheKey();
     const caseType = caseTypes.items?.find(x => x.code == code);
