7f5808183b495d0b595f8417421162b8a60f3da8
Constantinos Leftheris <c.leftheris@live.com>
2024-06-26 20:15:21 +0300

    slugify path tests correct ELOT expected result

--- test/Indice.Features.Messages.Tests/SlugifyPathTests.cs
+++ test/Indice.Features.Messages.Tests/SlugifyPathTests.cs
@@ -7,9 +7,9 @@ public class SlugifyPathTests
 {
     [InlineData("Λάχανο Ελληνικό Ποιότητα Α΄ Τιμή Κιλού", "lachano-elliniko-poiotita-a-timi-kilou")]
     [InlineData("Κρεμμύδια Ξερά Χοντρά Τιμή Κιλού", "kremmydia-xera-chontra-timi-kilou")]
-    [InlineData("Μαναβικη ", "manavikh")]
-    [InlineData("Μαναβικη/Λαχανικά", "manavikh/laxanika")]
-    [InlineData("Μαναβικη/Λαχανικά/Καρότα, Ραπανάκια, Τζίντζερ", "manavikh/laxanika/karota-rapanakia-tzintzer")]
+    [InlineData("Μαναβικη ", "manaviki")]
+    [InlineData("Μαναβικη/Λαχανικά", "manaviki/lachanika")]
+    [InlineData("Μαναβικη/Λαχανικά/Καρότα, Ραπανάκια, Τζίντζερ", "manaviki/lachanika/karota-rapanakia-tzintzer")]
     [Theory]
     public void GreekOrUnicodeMustConvertToMeaningfull_ASCII_Test(string input, string expected) {
         
