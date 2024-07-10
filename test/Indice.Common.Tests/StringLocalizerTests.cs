218da231499ad6db2ab1f79ea51855b85a9afc03
Constantinos Leftheris <c.leftheris@live.com>
2024-07-10 19:09:43 +0300

    String localizer graph test

---
+++ test/Indice.Common.Tests/StringLocalizerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Indice.Localization;
+using Xunit;
+
+namespace Indice.Common.Tests;
+public class StringLocalizerTests
+{
+
+    [Fact(Skip = "not ready")]
+    public void Resx_ResourcesShouldExportAsJsonGraph() { 
+
+        var graph = StringLocalizerTestsTranslations.ResourceManager.ToObjectGraph(new CultureInfo("el"), '.');
+        var json = JsonSerializer.Serialize(graph);
+    }
+}
