b6ec5c8c3efc2f66f136ced965c9b33518f0831c
Napoleon Michilis <n.michilis@indice.gr>
2024-10-24 13:36:39 +0300

    added profile picture, profile card and crop functionality in admin ui

--- src/Indice.Features.Identity.AdminUI.App/src/app/features/users/edit/user-edit.component.ts
+++ src/Indice.Features.Identity.AdminUI.App/src/app/features/users/edit/user-edit.component.ts
@@ -1,28 +1,336 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
-import { LoggerService } from 'src/app/core/services/logger.service';
-import { UserStore } from './user-store.service';
-import { IdentityApiService, UiFeaturesInfo } from 'src/app/core/services/identity-api.service';
-import { UiFeaturesService } from 'src/app/core/services/ui-features.service';
+import { LoggerService } from "src/app/core/services/logger.service";
+import { UserStore } from "./user-store.service";
+import {
+  FileParameter,
+  IdentityApiService,
+  SingleUserInfo,
+  UiFeaturesInfo,
+} from "src/app/core/services/identity-api.service";
+import { UiFeaturesService } from "src/app/core/services/ui-features.service";
+import { forkJoin, map, Subscription } from "rxjs";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-    selector: 'app-user-edit',
-    templateUrl: './user-edit.component.html',
-    providers: [UserStore]
+  selector: "app-user-edit",
+  templateUrl: "./user-edit.component.html",
+  styleUrls: ["./user-edit.component.scss"],
+  providers: [UserStore],
 })
-export class UserEditComponent implements OnInit {
-    constructor(private route: ActivatedRoute, private logger: LoggerService, private uiFeaturesService: UiFeaturesService) { }
+export class UserEditComponent implements OnInit, OnDestroy {
+  @ViewChild("content", { static: false })
+  contentRef!: TemplateRef<HTMLDivElement>;
+  @ViewChild("fileInput", { static: true })
+  fileInputRef!: ElementRef<HTMLInputElement>;
 
-    public userId = '';
-    public signInLogsEnabled = false;
+  public userId = "";
+  public userName = "";
+  public displayName = "";
+  public avatar = "";
+  public signInLogsEnabled = false;
 
-    public ngOnInit(): void {
-        this.logger.log('UserEditComponent ngOnInit was called.');
-        this.userId = this.route.snapshot.params['id'];
+  public modalRef: NgbModalRef;
+  public canvas: HTMLCanvasElement;
 
-        this.uiFeaturesService.getUiFeatures().subscribe((response: UiFeaturesInfo) => {
-            this.signInLogsEnabled = response.signInLogsEnabled;
-        });
+  public scale = 1;
+  public minScale = 1;
+  public maxScale = 1;
+
+  public canvasWidth = 400;
+  public canvasHeight = 400;
+
+  private ctx!: CanvasRenderingContext2D;
+  private img!: HTMLImageElement;
+
+  private panX = 0;
+  private panY = 0;
+
+  private posX = 0;
+  private posY = 0;
+  private isPanning = false;
+  private initialDistance = 0;
+
+  private _getDataSubscription: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private logger: LoggerService,
+    private uiFeaturesService: UiFeaturesService,
+    private userStore: UserStore,
+    private apiService: IdentityApiService,
+    private modalService: NgbModal
+  ) {}
+
+  public ngOnInit(): void {
+    this.logger.log("UserEditComponent ngOnInit was called.");
+    this.userId = this.route.snapshot.params["id"];
+
+    const getFeatures = this.uiFeaturesService.getUiFeatures();
+    const getUser = this.userStore.getUser(this.userId);
+
+    this._getDataSubscription = forkJoin([getFeatures, getUser])
+      .pipe(
+        map((responses: [UiFeaturesInfo, SingleUserInfo]) => {
+          return {
+            features: responses[0],
+            user: responses[1],
+          };
+        })
+      )
+      .subscribe(
+        (result: { user: SingleUserInfo; features: UiFeaturesInfo }) => {
+          this.signInLogsEnabled = result.features.signInLogsEnabled;
+
+          const { userName, claims } = result.user;
+          const givenName = claims.find((c) => c.type === "given_name");
+          const familyName = claims.find((c) => c.type === "family_name");
+          const avatar = claims.find((c) => c.type === "picture");
+          this.userName = userName;
+          this.displayName = `${givenName.value} ${familyName.value}`;
+          this.avatar =
+            avatar?.value ||
+            `https://localhost:2000/pictures/${this.userId}/128?d=/avatar/${this.displayName}/128.png`;
+        }
+      );
+  }
+
+  public ngOnDestroy(): void {
+    this.logger.log("UserEditComponent ngOnDestroy was called.");
+    if (this._getDataSubscription) {
+      this._getDataSubscription.unsubscribe();
     }
+  }
+
+  public openModal(): void {
+    this.modalRef = this.modalService.open(this.contentRef);
+    this.modalRef.shown.subscribe((_) => {
+      this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
+      this.ctx = this.canvas.getContext("2d")!;
+
+      this.drawImage();
+    });
+  }
+
+  public closeModal(): void {
+    if (this.modalRef) {
+      this.modalRef.close(0);
+    }
+
+    this.resetImagePosition();
+    this.scale = this.minScale;
+    this.panX = 0;
+    this.panY = 0;
+
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.fileInputRef.nativeElement.value = "";
+  }
+
+  public onFileChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files ? input.files[0] : null;
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        this.img = new Image();
+        this.img.src = e.target?.result as string;
+        this.img.onload = () => {
+          this.calculateMinAndMaxScale();
+          this.resetImagePosition();
+          // this.drawImage();
+
+          this.openModal();
+        };
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  public calculateMinAndMaxScale(): void {
+    const scaleWidth = this.img.width / this.canvasWidth;
+    const scaleHeight = this.img.height / this.canvasHeight;
+
+    // const isLoRes =
+    //   this.img.width <= this.canvasWidth &&
+    //   this.img.height <= this.canvasHeight;
+
+    // this.minScale = 1;
+    // if (this.img.width === this.img.height) {
+    this.minScale = Math.min(
+      this.canvasWidth / this.img.width,
+      this.canvasHeight / this.img.height
+    );
+    // }
+    // if (isLoRes) {
+    //   this.minScale = Math.min(
+    //     this.canvasWidth / this.img.width,
+    //     this.canvasHeight / this.img.height
+    //   );
+    // }
+    this.maxScale = Math.max(scaleWidth, scaleHeight);
+    this.scale = this.minScale;
+  }
+
+  public resetImagePosition(): void {
+    this.panX = (this.canvasWidth - this.img.width * this.scale) / 2;
+    this.panY = (this.canvasHeight - this.img.height * this.scale) / 2;
+  }
+
+  public drawImage(): void {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.save();
+    this.ctx.translate(this.panX, this.panY);
+    this.ctx.scale(this.scale, this.scale);
+    this.ctx.drawImage(this.img, 0, 0);
+    this.ctx.restore();
+  }
+
+  public onZoomChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const newScale = parseFloat(target.value);
+
+    this.zoomAtCenter(newScale);
+  }
+
+  public zoomAtCenter(newScale: number): void {
+    const canvasCenterX = this.canvas.width / 2;
+    const canvasCenterY = this.canvas.height / 2;
+
+    const imgCenterX = (canvasCenterX - this.panX) / this.scale;
+    const imgCenterY = (canvasCenterY - this.panY) / this.scale;
+
+    this.scale = Math.min(this.maxScale, Math.max(this.minScale, newScale));
+    this.panX = canvasCenterX - imgCenterX * this.scale;
+    this.panY = canvasCenterY - imgCenterY * this.scale;
+
+    this.drawImage();
+  }
+
+  public onPanStart(event: MouseEvent | TouchEvent): void {
+    if (event instanceof MouseEvent) {
+      this.isPanning = true;
+      this.posX = event.clientX;
+      this.posY = event.clientY;
+    } else if (event instanceof TouchEvent && event.touches.length === 1) {
+      this.isPanning = true;
+      this.posX = event.touches[0].clientX;
+      this.posY = event.touches[0].clientY;
+    } else if (event instanceof TouchEvent && event.touches.length === 2) {
+      this.initialDistance = this.getTouchDistance(event);
+    }
+  }
+
+  public onPan(event: MouseEvent | TouchEvent): void {
+    event.preventDefault();
+
+    if (this.isPanning) {
+      let dx = 0;
+      let dy = 0;
+
+      if (event instanceof MouseEvent) {
+        dx = event.clientX - this.posX;
+        dy = event.clientY - this.posY;
+
+        this.posX = event.clientX;
+        this.posY = event.clientY;
+      } else if (event instanceof TouchEvent && event.touches.length === 1) {
+        dx = event.touches[0].clientX - this.posX;
+        dy = event.touches[0].clientY - this.posY;
+
+        this.posX = event.touches[0].clientX;
+        this.posY = event.touches[0].clientY;
+      }
+
+      this.panX += dx;
+      this.panY += dy;
+      this.drawImage();
+    } else if (event instanceof TouchEvent && event.touches.length === 2) {
+      const currentDistance = this.getTouchDistance(event);
+      const scaleChange = currentDistance / this.initialDistance;
+      this.zoomAtCenter(this.scale * scaleChange);
+      this.initialDistance = currentDistance;
+    }
+  }
+
+  @HostListener("window:mouseup")
+  @HostListener("window:touchend")
+  public onPanEnd(): void {
+    this.isPanning = false;
+  }
+
+  public getTouchDistance(event: TouchEvent): number {
+    const touch1 = event.touches[0];
+    const touch2 = event.touches[1];
+
+    const dx = touch1.clientX - touch2.clientX;
+    const dy = touch1.clientY - touch2.clientY;
+
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  public cropImage(): void {
+    const cropWidth = this.canvasWidth / this.scale;
+    const cropHeight = this.canvasHeight / this.scale;
+
+    const cropX = -this.panX / this.scale;
+    const cropY = -this.panY / this.scale;
+
+    const offScreenCanvas = document.createElement("canvas");
+    offScreenCanvas.width = cropWidth;
+    offScreenCanvas.height = cropHeight;
+    const offScreenCtx = offScreenCanvas.getContext("2d")!;
+
+    offScreenCtx.drawImage(
+      this.img,
+      cropX,
+      cropY,
+      cropWidth,
+      cropHeight,
+      0,
+      0,
+      cropWidth,
+      cropHeight
+    );
+
+    const croppedImageURL = offScreenCanvas.toDataURL("image/png", 1.0);
+    this.upload(croppedImageURL);
+    // this.profileImageUrl = croppedImageURL;
+  }
+
+  private upload(imageData: string) {
+    const blob = this.dataURLtoBlob(imageData);
+    const formData = new FormData();
+    formData.append("file", blob, "canvas-image.png");
+
+    this.apiService
+      .updateUserPicture(this.userId, formData)
+      .subscribe((response) => {
+        console.log(response);
+        this.avatar = imageData;
+        this.closeModal();
+      });
+  }
+
+  private dataURLtoBlob(dataUrl) {
+    const byteString = atob(dataUrl.split(",")[1]);
+    const mimeString = dataUrl.split(",")[0].split(":")[1].split(";")[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ab], { type: mimeString });
+  }
 }
--- src/Indice.Features.Identity.AdminUI.App/src/app/layout/components/dashboard-shell/top-bar/top-bar.component.ts
+++ src/Indice.Features.Identity.AdminUI.App/src/app/layout/components/dashboard-shell/top-bar/top-bar.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { AuthService } from 'src/app/core/services/auth.service';
-import { MenuService } from 'src/app/layout/services/menu.service';
+import { AuthService } from "src/app/core/services/auth.service";
+import { MenuService } from "src/app/layout/services/menu.service";
 
 @Component({
-    selector: 'app-top-bar',
-    templateUrl: './top-bar.component.html'
+  selector: "app-top-bar",
+  templateUrl: "./top-bar.component.html",
 })
 export class TopBarComponent implements OnInit {
-    constructor(
-        public authService: AuthService,
-        private _menuService: MenuService
-    ) { }
+  constructor(
+    public authService: AuthService,
+    private _menuService: MenuService
+  ) {}
 
-    public displayName: string;
+  public picture: string;
+  public displayName: string;
 
-    public ngOnInit(): void {
-        this.displayName = this.authService.getDisplayName();
-    }
+  public ngOnInit(): void {
+    const profile = this.authService.getUserProfile();
+    this.picture = profile.picture;
+    this.displayName = this.authService.getDisplayName();
+  }
 
-    public signOut(): void {
-        this.authService.signoutRedirect();
-    }
+  public signOut(): void {
+    this.authService.signoutRedirect();
+  }
 
-    public toggleMenu(): void {
-        this._menuService.toggleSideMenu();
-    }
+  public toggleMenu(): void {
+    this._menuService.toggleSideMenu();
+  }
 }
