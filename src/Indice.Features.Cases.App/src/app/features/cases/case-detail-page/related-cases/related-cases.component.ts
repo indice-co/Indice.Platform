fc65a5502a66c90b525bcea3cdf41c62860ada79
Konstantinos Argyropoulos <k.argyropoulos@indice.gr>
2024-11-19 17:59:02 +0200

    Clean up

--- src/Indice.Features.Cases.App/src/app/features/cases/case-detail-page/related-cases/related-cases.component.ts
+++ src/Indice.Features.Cases.App/src/app/features/cases/case-detail-page/related-cases/related-cases.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CasePartial, CasesApiService } from 'src/app/core/services/cases-api.service';
+import { CasePartial } from 'src/app/core/services/cases-api.service';
 
 @Component({
   selector: 'app-case-related',
@@ -11,13 +11,12 @@ export class RelatedCasesComponent implements OnInit {
   @Input() relatedCases: CasePartial[] = [];
   currentCaseId: string = "";
 
-  constructor(private api: CasesApiService, private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     // From the related cases, remove the currently opened case from the list
     this.route.paramMap.subscribe(params => {
       this.currentCaseId = params.get('caseId') ?? "";
-      console.log(this.relatedCases);
     });
   }
 }
