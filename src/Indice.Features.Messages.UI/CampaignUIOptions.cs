cf1e7968c5032661835a0197103bb51eb1a72026
Constantinos Leftheris <c.leftheris@live.com>
2024-11-21 17:45:27 +0200

    messaging: media relative to ApiBaseUrl

--- src/Indice.Features.Messages.UI/CampaignUIOptions.cs
+++ src/Indice.Features.Messages.UI/CampaignUIOptions.cs
@@ -10,17 +10,18 @@ public class CampaignUIOptions : SpaUIOptions
     public bool EnableMediaLibrary { get; set; } = false;
     /// <summary>Enables a rich text editor for Campaign content.</summary>
     public bool EnableRichTextEditor { get; set; } = false;
-    /// <summary>Enables a rich text editor for Campaign content.</summary>
-    public string ApiUrl { get; set; } = "/api";
 #endif
+    /// <summary>Enables a rich text editor for Campaign content. defaults to <strong>/api</strong> which is the same origin. </summary>
+    /// <remarks>Can also be pointed to a an exteranl origin ei: messaging.indice.gr.</remarks>
+    public string ApiBaseUrl { get; set; } = "/api";
     /// <summary>Creates a new instance <see cref="CampaignUIOptions"/>.</summary>
     public CampaignUIOptions() {
         ConfigureIndexParameters = (args) => {
 #if NET7_0_OR_GREATER
             args.Add($"%({nameof(EnableMediaLibrary)})", EnableMediaLibrary.ToString());
             args.Add($"%({nameof(EnableRichTextEditor)})", EnableRichTextEditor.ToString());
-            args.Add($"%({nameof(ApiUrl)})", ApiUrl);
 #endif
+            args.Add($"%({nameof(ApiBaseUrl)})", ApiBaseUrl);
         };
     }
 }
