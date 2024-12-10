53a02f9b140693f0a08a332360bb4f61037b6d0a
Constantinos Leftheris <c.leftheris@live.com>
2024-12-10 11:16:02 +0200

    [cases] create case authorization extensions static class to hold requirements for building an authorizastion policy

---
+++ src/Indice.Features.Cases.Core/Authorization/CasesAuthorizationExtensinos.cs
@@ -0,0 +1,9 @@
+﻿namespace Indice.Features.Cases.Core.Authorization;
+
+/// <summary>
+/// Cases extensions on IAuthorization
+/// </summary>
+public static class CasesAuthorizationExtensinos
+{
+
+}
