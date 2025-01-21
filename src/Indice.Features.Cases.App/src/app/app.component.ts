4f46fd57b406eb576ba04104d483d3b4ec6b8dbe
Konstantinos Argyropoulos <k.argyropoulos@indice.gr>
2025-01-21 11:30:24 +0200

    Make loading bar take the full length of the screen

--- src/Indice.Features.Cases.App/src/app/app.component.ts
+++ src/Indice.Features.Cases.App/src/app/app.component.ts
@@ -4,18 +4,17 @@ import { LoadingBarService } from './core/services/loading-bar.service';
 
 @Component({
   selector: 'app-root',
-
   template: `
-    <lib-progress-bar
-    style="left: 16rem; position: fixed; width: calc(100% - 16rem); top: 0px; z-index: 200;"
-    [busy]="(loadingBarService.busy$ | async) ?? false"
-    [value]="(loadingBarService.value$ | async) ?? 0"
-    [total]="(loadingBarService.total$ | async) ?? 100"
-    [showBarOnly]="true"
-    text="Loading...">
-    </lib-progress-bar>
-    <lib-shell-layout></lib-shell-layout>
-  `
+  <lib-progress-bar
+  style="left: 0; position: fixed; width: 100%; top: 0px; z-index: 200;"
+  [busy]="(loadingBarService.busy$ | async) ?? false"
+  [value]="(loadingBarService.value$ | async) ?? 0"
+  [total]="(loadingBarService.total$ | async) ?? 100"
+  [showBarOnly]="true"
+  text="Loading...">
+  </lib-progress-bar>
+  <lib-shell-layout></lib-shell-layout>
+`
 })
 export class AppComponent {
   constructor(translate: TranslateService, public loadingBarService: LoadingBarService) {
