de3b0057a190c866d9915f05465fc6dfae184e9b
CostaArg <costaargdev@gmail.com>
2025-01-28 17:52:27 +0200

    [Messages] Add ngx progressbar

--- src/Indice.Features.Messages.App/src/app/app.component.ts
+++ src/Indice.Features.Messages.App/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { IAppSettings } from './core/models/settings.model';
 @Component({
   selector: 'app-root',
   template: `
+    <ng-progress ngProgressHttp/>
     <lib-shell-layout [sidebarFooterTemplate]="sidebarFooter"></lib-shell-layout>
     <ng-template #sidebarFooter>
       <span>
         Powered by <a class="text-blue-400" href="https://www.indice.gr">Indice</a>
-        <span class="ml-1" style="color: red">♥</span> 
+        <span class="ml-1" style="color: red">♥</span>
       </span> v{{ settings.version }}
     </ng-template>
   `
