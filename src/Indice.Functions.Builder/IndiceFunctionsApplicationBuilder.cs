bdb7dce95456ba511dd227502c612c7c6037d660
travlos <travlos7@hotmail.com>
2025-02-03 17:38:05 +0200

    Add Indice Functions builder (#554)
    * Indice Functions builder
    
    * Fix CodeQL warning

---
+++ src/Indice.Functions.Builder/IndiceFunctionsApplicationBuilder.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.Hosting;
+
+/// <summary>
+/// A builder for web applications and services that has all the indice defaults preconfigured. This is a decorator for the inner <seealso cref="FunctionsApplicationBuilder"/>
+/// </summary>
+public class IndiceFunctionsApplicationBuilder : IHostBuilder
+{
+    private HostBuilder InnerBuilder { get; }
+    
+
+    /// <summary>
+    /// constructs the <see cref="IndiceFunctionsApplicationBuilder "/> given the inner builder.
+    /// </summary>
+    /// <param name="innerBuilder"></param>
+    internal IndiceFunctionsApplicationBuilder(HostBuilder innerBuilder) {
+        InnerBuilder = innerBuilder;
+    }
+
+    /// <summary>
+    /// Builds the <see cref="IHost"/> to the idice specifications.
+    /// </summary>
+    /// <returns>A configured <see cref="IHost"/>.</returns>
+    public IHost Build() {
+        var app = InnerBuilder.Build();
+        return app;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IHostBuilder"/> class with preconfigured defaults.
+    /// </summary>
+    /// <returns>The <see cref="IHostBuilder"/>.</returns>
+    public static IHostBuilder CreateBuilder() {
+        var builder = new HostBuilder();
+        builder.ConfigureFunctionsDefaults();
+        return new IndiceFunctionsApplicationBuilder(builder);
+    }
+
+    /// <inheritdoc/>
+    public IDictionary<object, object> Properties => InnerBuilder.Properties;
+    /// <inheritdoc/>
+    public IHostBuilder ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> configureDelegate) => InnerBuilder.ConfigureAppConfiguration(configureDelegate);
+    /// <inheritdoc/>
+    public IHostBuilder ConfigureContainer<TContainerBuilder>(Action<HostBuilderContext, TContainerBuilder> configureDelegate) => InnerBuilder.ConfigureContainer(configureDelegate);
+    /// <inheritdoc/>
+    public IHostBuilder ConfigureHostConfiguration(Action<IConfigurationBuilder> configureDelegate) => InnerBuilder.ConfigureHostConfiguration(configureDelegate);
+    /// <inheritdoc/>
+    public IHostBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configureDelegate) => InnerBuilder.ConfigureServices(configureDelegate);
+    /// <inheritdoc/>
+    public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory) where TContainerBuilder : notnull => InnerBuilder.UseServiceProviderFactory(factory);
+    /// <inheritdoc/>
+    public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(Func<HostBuilderContext, IServiceProviderFactory<TContainerBuilder>> factory) where TContainerBuilder : notnull => InnerBuilder.UseServiceProviderFactory(factory);
+}
---
+++ src/Indice.Functions.Builder/ServiceDefaultsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Indice.Functions.Builder;
+using Microsoft.Azure.Functions.Worker.Builder;
+
+namespace Microsoft.Extensions.Hosting;
+/// <summary>
+/// Extension methods to configure resiliency and telemetry settings.
+/// Services include OpenTelemetry, HttpClient default resiliency, Healthcheks and their respective /health, /alive endpoints
+/// </summary>
+public static class ServiceDefaultsExtensions
+{
+    /// <summary>
+    /// Adds webserive defaults for ConfigureOpenTelemetry, AddDefaultHealthChecks, ConfigureHttpClientDefaults
+    /// </summary>
+    /// <param name="builder">The builder to configure</param>
+    /// <returns>The <see cref="IHostApplicationBuilder"/> for further configuration.</returns>
+    public static IHostBuilder ConfigureFunctionsDefaults(this IHostBuilder builder) {
+        builder.ConfigureServices((context, services) => {
+            services.AddWorkerServiceOpenTelemetry(context);
+            services.AddFunctionHealthChecks();
+        });
+        return builder;
+    }
+
+}
