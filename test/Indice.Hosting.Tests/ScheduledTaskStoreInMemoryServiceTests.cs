46bb74f4b343cf18626e3efd03f4498682df8380
Eleftherios Rentas <e.rentas@indice.gr>
2025-02-04 18:03:34 +0200

    [Hosting] Remove unneeded packages in testing project

--- test/Indice.Hosting.Tests/ScheduledTaskStoreInMemoryServiceTests.cs
+++ test/Indice.Hosting.Tests/ScheduledTaskStoreInMemoryServiceTests.cs
@@ -8,12 +8,12 @@ namespace Indice.Hosting.Tests;
 
 public class ScheduledTaskStoreInMemoryServiceTests
 {
-    private const int TEST_TASK_EXECUTION_COUNT = 10;
-    private const string _testTaskCronExpression = "* * * ? * * *";
-    private const string _testTask2CronExpression = "* * * ? * * *";
+    private const int TestTaskExecutionCount = 10;
+    private const string TestTaskCronExpression = "* * * ? * * *";
+    private const string TestTask2CronExpression = "* * * ? * * *";
     private readonly IWebHost _host;
     private static int _counter = -1; // We have to initialize this at -1 since the first run is immediate.
-    private static int _countdownCounter = TEST_TASK_EXECUTION_COUNT + 1; // We have to initialize this at TEST_TASK_EXECUTION_COUNT + 1 since the first run is immediate.
+    private static int _countdownCounter = TestTaskExecutionCount + 1; // We have to initialize this at TEST_TASK_EXECUTION_COUNT + 1 since the first run is immediate.
 
     public ScheduledTaskStoreInMemoryServiceTests() {
         var builder = new WebHostBuilder();
@@ -23,14 +23,14 @@ public class ScheduledTaskStoreInMemoryServiceTests
                 options.UseScheduledTaskStoreInMemory();
                 })
             .AddJob<TestTask>()
-            .WithScheduleTrigger(_testTaskCronExpression, options => {
+            .WithScheduleTrigger(TestTaskCronExpression, options => {
                 options.Singleton = true;
                 options.Name = nameof(TestTask);
                 options.Group = nameof(TestTask);
                 options.Description = "Asserts true. Is a singleton";
             })
             .AddJob<TestTask2>()
-            .WithScheduleTrigger(_testTask2CronExpression, options => {
+            .WithScheduleTrigger(TestTask2CronExpression, options => {
                 options.Name = nameof(TestTask2);
                 options.Group = nameof(TestTask2);
                 options.Description = "Asserts true. Is not a singleton";
@@ -49,9 +49,9 @@ public class ScheduledTaskStoreInMemoryServiceTests
 
     [Fact]
     public async Task ScheduledTaskStoreInMemory_Runs_ReadyForExecutionTasks() {
-        await Task.Delay(TEST_TASK_EXECUTION_COUNT * 1000);
+        await Task.Delay(TestTaskExecutionCount * 1000);
         await _host.StopAsync();
-        Assert.Equal(TEST_TASK_EXECUTION_COUNT, _counter);
+        Assert.Equal(TestTaskExecutionCount, _counter);
         Assert.Equal(0, _countdownCounter);
     }
 
