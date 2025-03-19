2b656b0377567993295d8aaabc9e1c50730d1823
Constantinos Leftheris <c.leftheris@live.com>
2025-03-19 15:29:33 +0200

    Suport gsis login with classic oAuth

--- src/Indice.AspNetCore.Authentication.GovGr/GovGrDefaults.cs
+++ src/Indice.AspNetCore.Authentication.GovGr/GovGrDefaults.cs
@@ -8,13 +8,15 @@ public class GovGrDefaults
     /// <summary>The default display name for gov.gr authentication. Defaults to <c>Sign in with GovGr</c>.</summary>
     public static readonly string DisplayName = "Sign in with GovGr";
     /// <summary>The default endpoint used to perform gov.gr authentication.</summary>
-    public static readonly string AuthorizationEndpoint = "https://kyc.gov.gr/oauth";
+    public static readonly string AuthorizationEndpoint = "https://oauth2.gsis.gr/oauth2server/oauth/authorize";
     /// <summary>The OAuth endpoint used to exchange access tokens.</summary>
-    public static readonly string TokenEndpoint = "https://kyc.gov.gr/oauth/token";
+    public static readonly string TokenEndpoint = "https://oauth2.gsis.gr/oauth2server/oauth/token";
     /// <summary>The OAuth endpoint used to discover OpenId connect supported features.</summary>
     public static readonly string DiscoveryEndpoint = string.Empty;
     /// <summary>The authority.</summary>
-    public static readonly string Authority = "https://kyc.gov.gr";
+    public static readonly string Authority = "https://oauth2.gsis.gr";
     /// <summary>No user info endpoint is provided, which means all of the claims about users have to be included in the (expiring and potentially large) id_token.</summary>
-    public static readonly string UserInformationEndpoint = string.Empty;
+    public static readonly string UserInformationEndpoint = "https://oauth2.gsis.gr/oauth2server/userinfo";
+
 }
+
