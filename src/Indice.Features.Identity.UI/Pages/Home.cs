ca9a28a2a087dc3607f6182c65db535d4512bc6a
Constantinos Leftheris <c.leftheris@live.com>
2025-04-22 15:37:36 +0300

    [Identity] Mfa deadlock message handling instead of unhandled exception.

--- src/Indice.Features.Identity.UI/Pages/Home.cs
+++ src/Indice.Features.Identity.UI/Pages/Home.cs
@@ -8,22 +8,19 @@ using Microsoft.Extensions.Configuration;
 namespace Indice.Features.Identity.UI.Pages;
 
 /// <summary>Page model for the home page screen.</summary>
+/// <remarks>Creates a new instance of <see cref="BaseLoginModel"/> class.</remarks>
+/// <param name="configuration">Represents a set of key/value application configuration properties.</param>
+/// <exception cref="ArgumentNullException"></exception>
 [IdentityUI(typeof(HomeModel))]
 [SecurityHeaders]
-public abstract class BaseHomeModel : BasePageModel
+public abstract class BaseHomeModel(IConfiguration configuration) : BasePageModel
 {
-    /// <summary>Creates a new instance of <see cref="BaseLoginModel"/> class.</summary>
-    /// <param name="configuration">Represents a set of key/value application configuration properties.</param>
-    /// <exception cref="ArgumentNullException"></exception>
-    public BaseHomeModel(IConfiguration configuration) {
-        Configuration = configuration;
-    }
 
     /// <summary>Represents a set of key/value application configuration properties.</summary>
-    protected IConfiguration Configuration { get; }
+    protected IConfiguration Configuration { get; } = configuration;
 
     /// <summary></summary>
-    public List<GatewayServiceModel> Services { get; set; } = new List<GatewayServiceModel>();
+    public List<GatewayServiceModel> Services { get; set; } = [];
 
     /// <summary>Home page GET handler.</summary>
     public virtual async Task<IActionResult> OnGetAsync() {
@@ -42,7 +39,6 @@ public abstract class BaseHomeModel : BasePageModel
     }
 }
 
-internal class HomeModel : BaseHomeModel
+internal class HomeModel(IConfiguration configuration) : BaseHomeModel(configuration)
 {
-    public HomeModel(IConfiguration configuration) : base(configuration) { }
 }
