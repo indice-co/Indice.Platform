07fd4b226850e3e20f5caee215ca86360d626c5a
travlos <travlos7@hotmail.com>
2025-05-15 12:22:12 +0300

    Enhance TryGetLastActor to support HTTP request input
    Updated the `TryGetLastActor` method in `ActivityExecutionContextExtensions.cs` to retrieve the last actor from the HTTP request body if an "actor" field is present. If not, it falls back to the Workflow context variable "RunAsSystemUser" and the last actor from the Variables. Adjusted using directives and updated comments to reflect these changes.

--- src/Indice.Features.Cases.Workflows/Extensions/ActivityExecutionContextExtensions.cs
+++ src/Indice.Features.Cases.Workflows/Extensions/ActivityExecutionContextExtensions.cs
@@ -1,20 +1,31 @@
-﻿using System.Security.Claims;
+﻿using Elsa.Activities.Http.Models;
+using System.Text.Json.Nodes;
 using Elsa.Services.Models;
-using Indice.Features.Cases.Workflows.Integrations;
 using Indice.Features.Cases.Workflows.Models;
-using Microsoft.AspNetCore.Http;
+using System.Text.Json;
 
 namespace Indice.Features.Cases.Workflows.Extensions;
 
 /// <summary>Extensions for <see cref="ActivityExecutionContext"/>.</summary>
 public static class ActivityExecutionContextExtensions
 {
-    /// <summary>Try to get the last actor from the Workflow context variable "RunAsSystemUser" or the Last Actor from the Variables.</summary>
+    /// <summary>Try to get the last actor either from the HTTP request body with an "actor" field
+    /// or the Workflow context variable "RunAsSystemUser" or the Last Actor from the Variables.</summary>
     public static Actor TryGetLastActor(this ActivityExecutionContext context) {
+        if (context.Input is HttpRequestModel { RawBody: not null } request) {
+            var body = JsonNode.Parse(request.RawBody);
+            var actorNode = body?["actor"];
+            var tempActor = actorNode?.Deserialize<Actor>();
+            if (tempActor is not null) {
+                return tempActor;
+            }
+        }
+
         var runAsSystemUser = context.GetVariable<bool>("RunAsSystemUser");
-        return runAsSystemUser
-            ? Actor.Create(CasesClaimsPrincipalExtensions.SystemUser())
-            : context.GetVariable<Actor>(CasesWorkflowConstants.WorkflowVariables.Actor.Current) 
-              ?? Actor.Create(CasesClaimsPrincipalExtensions.SystemUser()); // TODO[2025-07-07]: Remove this eventually
+        if (runAsSystemUser) {
+            return Actor.Create(CasesClaimsPrincipalExtensions.SystemUser());
+        }
+        return context.GetVariable<Actor>(CasesWorkflowConstants.WorkflowVariables.Actor.Current)
+            ?? Actor.Create(CasesClaimsPrincipalExtensions.SystemUser()); // TODO[2025-07-07]: Remove this eventually
     }
 }
