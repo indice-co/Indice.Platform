f6d553afebb4fa71f63928102d07205ea1910b54
travlos <travlos7@hotmail.com>
2025-05-15 15:35:02 +0300

    Add TrySetLastActor method and improve documentation
    Implemented TrySetLastActor in HttpEndpointWithValidation.cs to set the last actor from the HTTP request body. Updated ExecuteInternal to utilize this new method. Enhanced TryGetLastActor in ActivityExecutionContextExtensions.cs for better logic and documentation clarity.

--- src/Indice.Features.Cases.Workflows/Extensions/ActivityExecutionContextExtensions.cs
+++ src/Indice.Features.Cases.Workflows/Extensions/ActivityExecutionContextExtensions.cs
@@ -9,24 +9,38 @@ namespace Indice.Features.Cases.Workflows.Extensions;
 /// <summary>Extensions for <see cref="ActivityExecutionContext"/>.</summary>
 public static class ActivityExecutionContextExtensions
 {
-    /// <summary>Try to get the last actor either from the HTTP request body with an "actor" field
-    /// or the Workflow context variable "RunAsSystemUser" or the Last Actor from the Variables.</summary>
-    public static Actor TryGetLastActor(this ActivityExecutionContext context) {
+    /// <summary>
+    /// Try to get the last actor either either from Workflow context variable "RunAsSystemUser", or the Last Actor from the Variables.
+    /// </summary>
+    /// <param name="context">The activity execution context.</param>
+    /// <returns>The last actor or a system user actor if none is found.</returns>
+    public static Actor TryGetLastActor(this ActivityExecutionContext context)
+    {
         var forceRunAsSystemUser = context.GetVariable<bool>("RunAsSystemUser");
-        if (forceRunAsSystemUser) {
+        if (forceRunAsSystemUser)
+        {
             return Actor.Create(CasesClaimsPrincipalExtensions.SystemUser());
         }
+        var actor = 
+        context.GetVariable<Actor>(CasesWorkflowConstants.WorkflowVariables.Actor.Current)
+            ?? Actor.Create(CasesClaimsPrincipalExtensions.SystemUser()); // TODO[2025-07-07]: Remove this eventually
+        return actor;
+    }
 
-        if (context.Input is HttpRequestModel { RawBody: not null } request) {
+    /// <summary>
+    /// Attempts to set the last actor in the workflow context by extracting it from the HTTP request body.
+    /// </summary>
+    /// <param name="context">The activity execution context.</param>
+    public static void TrySetLastActor(this ActivityExecutionContext context)
+    {
+        if (context.Input is HttpRequestModel { RawBody: not null } request)
+        {
             var body = JsonNode.Parse(request.RawBody);
             var actorNode = body?["actor"];
-            var tempActor = actorNode?.Deserialize<Actor>();
-            if (tempActor is not null) {
-                return tempActor;
+            var currentActor = actorNode?.Deserialize<Actor>();
+            if (currentActor is not null) {
+                context.SetVariable(CasesWorkflowConstants.WorkflowVariables.Actor.Current, currentActor);
             }
         }
-        
-        return context.GetVariable<Actor>(CasesWorkflowConstants.WorkflowVariables.Actor.Current)
-            ?? Actor.Create(CasesClaimsPrincipalExtensions.SystemUser()); // TODO[2025-07-07]: Remove this eventually
     }
 }
