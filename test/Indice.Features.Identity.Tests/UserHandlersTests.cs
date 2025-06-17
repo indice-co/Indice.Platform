60c52caa3ccdedfea0aa5c6cd33945fb03b486d1
travlos <travlos7@hotmail.com>
2025-06-17 17:58:08 +0300

    Test for get users method

--- test/Indice.Features.Identity.Tests/UserHandlersTests.cs
+++ test/Indice.Features.Identity.Tests/UserHandlersTests.cs
@@ -5,6 +5,9 @@ using Indice.Features.Identity.Core.Data.Models;
 using Indice.Features.Identity.Core.Data.Stores;
 using Indice.Features.Identity.Core.Events;
 using Indice.Features.Identity.Server.Manager;
+using Indice.Features.Identity.Server.Manager.Models;
+using Indice.Types;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,12 +20,12 @@ public class UserHandlersTests : IAsyncLifetime
 
     public UserHandlersTests() {
         var configuration = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string> {
-            ["test"] = "test"
+            ["ConnectionStrings:TestDb"] = $"Server=(localdb)\\MSSQLLocalDB;Database=Indice.FilterClause.Test_{Environment.Version.Major}_{Guid.NewGuid()};Trusted_Connection=True;MultipleActiveResultSets=true",
         }).Build();
         var services = new ServiceCollection();
         // configure dependencies
         services.AddSingleton<IConfiguration>(configuration);
-        services.AddDbContext<ExtendedIdentityDbContext<User, Role>>(builder => builder.UseInMemoryDatabase("IdentityDb"));
+        services.AddDbContext<ExtendedIdentityDbContext<User, Role>>(builder => builder.UseSqlServer(configuration.GetConnectionString("TestDb")));
         services.AddIdentity<User, Role>()
                    .AddExtendedUserManager()
                    .AddUserStore<ExtendedUserStore<ExtendedIdentityDbContext<User, Role>, User, Role>>()
@@ -39,12 +42,12 @@ public class UserHandlersTests : IAsyncLifetime
 
     [Fact]
     public async Task CreateUserHandler_ShouldEmit_UserCreatedEvent_WithPopulatedClaims_Test() {
-        
+
         var userManager = _serviceProvider.GetRequiredService<ExtendedUserManager<User>>();
         var identityDbContext = _serviceProvider.GetRequiredService<ExtendedIdentityDbContext<User, Role>>();
-        
+
         //seed database
-        identityDbContext.Roles.Add(new Role("Developer") { NormalizedName = "DEVELOPER"} );
+        identityDbContext.Roles.Add(new Role("Developer") { NormalizedName = "DEVELOPER" });
         await identityDbContext.SaveChangesAsync();
 
         // execute
@@ -55,21 +58,63 @@ public class UserHandlersTests : IAsyncLifetime
             BypassPasswordValidation = true,
             FirstName = "John",
             LastName = "Doe",
-            Claims = [ 
-                new() { Type = "customer_code", Value = "000001" }, 
+            Claims = [
+                new() { Type = "customer_code", Value = "000001" },
                 new() { Type = "locale", Value = "el" }
             ],
-            Roles = [ "Developer" ]
+            Roles = ["Developer"]
         });
         Assert.True(true);
     }
 
-    public async Task DisposeAsync() {
-        await _serviceProvider.DisposeAsync();
+
+
+    [Fact]
+    public async Task GetUsers_Test() {
+
+        var userManager = _serviceProvider.GetRequiredService<ExtendedUserManager<User>>();
+        //seed database
+        var identityDbContext = _serviceProvider.GetRequiredService<ExtendedIdentityDbContext<User, Role>>();
+
+        //seed database
+        identityDbContext.Roles.Add(new Role("Developer") { NormalizedName = "DEVELOPER" });
+        await identityDbContext.SaveChangesAsync();
+
+        // execute
+        _ = await UserHandlers.CreateUser(userManager, identityDbContext, new Server.Manager.Models.CreateUserRequest {
+            UserName = "john.doe@indice.gr",
+            Email = "john.doe@indice.gr",
+            Password = "password",
+            BypassPasswordValidation = true,
+            FirstName = "John",
+            LastName = "Doe",
+            Claims = [
+                new() { Type = "customer_code", Value = "000001" },
+                new() { Type = "locale", Value = "el" }
+            ],
+            Roles = ["Developer"]
+        });
+
+        ListOptions options = new() {
+            Page = 1,
+            Size = 10
+        };
+        UserListFilter filter = new();
+        // execute
+        _ = await UserHandlers.GetUsers(identityDbContext, options, filter, ["locale"]);
+        Assert.True(true);
     }
 
-    public Task InitializeAsync() {
-        return Task.CompletedTask;
+
+    public async Task InitializeAsync() {
+        var dbContext = _serviceProvider.GetRequiredService<ExtendedIdentityDbContext<User, Role>>();
+        await dbContext.Database.EnsureCreatedAsync();
+    }
+
+    public async Task DisposeAsync() {
+        var dbContext = _serviceProvider.GetRequiredService<ExtendedIdentityDbContext<User, Role>>();
+        await dbContext.Database.EnsureDeletedAsync();
+        await _serviceProvider.DisposeAsync();
     }
 
     public class UserCreatedAssetionHanbdler : IPlatformEventHandler<UserCreatedEvent>
