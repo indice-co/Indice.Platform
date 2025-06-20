00edb75d770d75306f711701e163eb2895465705
Constantinos Leftheris <c.leftheris@live.com>
2025-06-20 15:24:41 +0300

    Add HttpClientFactoryExtensions for loopback URI retrieval
    Introduces a new static class `HttpClientFactoryExtensions` in the `Microsoft.Extensions.DependencyInjection` namespace. This class includes an extension method `GetServerLoopbackUri` for `IServiceProvider`, which retrieves the server's loopback URI by replacing the host with "localhost" and determining the internal port. This enhancement facilitates making requests to the server without using the external network stack.

---
+++ src/Indice.AspNetCore/Http/Client/HttpClientFactoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>Extensions for <see cref="IHttpClientFactory"/>.</summary>
+public static class HttpClientFactoryExtensions
+{
+    /// <summary>
+    /// Retrieves the loopback URI for the server, replacing the host with "localhost" while finding out the internal port.
+    /// This makes it easier to make requests that talk to the server self without going through the external network stack.
+    /// </summary>
+    /// <param name="serviceProvider">The <see cref="IServiceProvider"/> instance used to resolve the server and its features.</param>
+    /// <returns>A <see cref="Uri"/> representing the server's loopback address with the host set to "localhost" and the internal port.</returns>
+    public static Uri GetServerLoopbackUri(this IServiceProvider serviceProvider) {
+        var address = serviceProvider.GetRequiredService<IServer>().Features.Get<IServerAddressesFeature>()!.Addresses.First();
+        var uriBuilder = new UriBuilder(address) {
+            Host = "localhost"
+        };
+        return uriBuilder.Uri;
+    }
+}
