ccb780d56a6e0cad5a675c89b5987c5643f16c72
Vasileios Spyridonos <spyridonosvasileios@gmail.com>
2025-07-10 15:10:11 +0300

    Remove commented code and change indentation

--- src/Indice.Features.Messages.App/src/app/shared/components/list-contact-create/list-contact-create.component.ts
+++ src/Indice.Features.Messages.App/src/app/shared/components/list-contact-create/list-contact-create.component.ts
@@ -5,28 +5,28 @@ import { settings } from 'src/app/core/models/settings';
 import { MessagesApiClient, Contact, ContactResultSet } from 'src/app/core/services/messages-api.service';
 
 @Component({
-  selector: 'app-list-contact-create',
-  templateUrl: './list-contact-create.component.html'
+    selector: 'app-list-contact-create',
+    templateUrl: './list-contact-create.component.html'
 })
 export class ListContactCreateComponent implements AfterViewInit {
 
-  @ViewChild('contactsCombobox', { static: false }) public contactsCombobox!: EnhancedComboboxComponent;
+    @ViewChild('contactsCombobox', { static: false }) public contactsCombobox!: EnhancedComboboxComponent;
 
-  @Output() onSubmit: EventEmitter<Contact[]> = new EventEmitter<Contact[]>();
-  @Output() onCancel: EventEmitter<Contact[]> = new EventEmitter<Contact[]>();
+    @Output() onSubmit: EventEmitter<Contact[]> = new EventEmitter<Contact[]>();
+    @Output() onCancel: EventEmitter<Contact[]> = new EventEmitter<Contact[]>();
 
-  constructor(
-      private _changeDetector: ChangeDetectorRef,
-      private _api: MessagesApiClient
-  ) { }
+    constructor(
+        private _changeDetector: ChangeDetectorRef,
+        private _api: MessagesApiClient
+    ) { }
 
-  public submitInProgress = false;
-  public contacts: Contact[] = [];
-  public isLoading: boolean = false;
-  public avatarOrigin = (settings.api_url || "").substring(0, 4) === "http" ? new URL(settings.api_url).origin : "";
-  public get anyContactEditing() {
-      return false;
-  }
+    public submitInProgress = false;
+    public contacts: Contact[] = [];
+    public isLoading: boolean = false;
+    public avatarOrigin = (settings.api_url || "").substring(0, 4) === "http" ? new URL(settings.api_url).origin : "";
+    public get anyContactEditing() {
+        return false;
+    }
     public savedContacts: Contact[] = [];
 
     public displayShowMoreOption: boolean = false;
@@ -82,53 +82,53 @@ export class ListContactCreateComponent implements AfterViewInit {
     }
 
 
-public onContactSelected(contact: Contact): void { }
+    public onContactSelected(contact: Contact): void { }
 
-public ngAfterViewInit(): void {
-    this._changeDetector.detectChanges();
-}
-
-public onContactSaveChanges(item: any): void {
-    delete item._edit;
-}
-
-public onAddNewContact(searchTerm: string): void {
-    const validateEmail = (email: string) => {
-        return String(email)
-            .toLowerCase()
-            .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    };
-    if (this.contactsCombobox.selectedItems.some(x => x.fullName === searchTerm)) {
-        return;
+    public ngAfterViewInit(): void {
+        this._changeDetector.detectChanges();
     }
-    const contact = new Contact();
-    searchTerm = searchTerm.trim()
-    if (validateEmail(searchTerm)) {
-        contact.email = searchTerm;
-    } else {
-        contact.fullName = searchTerm;
-        contact.firstName = searchTerm.split(' ')[0];
-        contact.lastName = searchTerm.slice(contact.firstName.length).trim();
+
+    public onContactSaveChanges(item: any): void {
+        delete item._edit;
     }
-    (<any>contact)._edit = true;
-  this.contactsCombobox.selectedItems.unshift(contact);
-  this.contactsCombobox.busy = true;
-}
 
-public submit(): void {
-    this.savedContacts = JSON.parse(JSON.stringify(this.contactsCombobox.selectedItems));
-    this.onSubmit.emit(this.contactsCombobox.selectedItems);
-}
+    public onAddNewContact(searchTerm: string): void {
+        const validateEmail = (email: string) => {
+            return String(email)
+                .toLowerCase()
+                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        };
+        if (this.contactsCombobox.selectedItems.some(x => x.fullName === searchTerm)) {
+            return;
+        }
+        const contact = new Contact();
+        searchTerm = searchTerm.trim()
+        if (validateEmail(searchTerm)) {
+            contact.email = searchTerm;
+        } else {
+            contact.fullName = searchTerm;
+            contact.firstName = searchTerm.split(' ')[0];
+            contact.lastName = searchTerm.slice(contact.firstName.length).trim();
+        }
+        (<any>contact)._edit = true;
+      this.contactsCombobox.selectedItems.unshift(contact);
+      this.contactsCombobox.busy = true;
+    }
 
-public cancel(): void {
-    this.contactsCombobox.selectedItems = JSON.parse(JSON.stringify(this.savedContacts));
-    this.onCancel.emit(this.contactsCombobox.selectedItems);
-}
+    public submit(): void {
+        this.savedContacts = JSON.parse(JSON.stringify(this.contactsCombobox.selectedItems));
+        this.onSubmit.emit(this.contactsCombobox.selectedItems);
+    }
 
-public reset(): void {
-    this.contactsCombobox.selectedItems = [];
-    this.savedContacts = [];
-}
+    public cancel(): void {
+        this.contactsCombobox.selectedItems = JSON.parse(JSON.stringify(this.savedContacts));
+        this.onCancel.emit(this.contactsCombobox.selectedItems);
+    }
 
-public onContactChangesSubmit(): void { }
+    public reset(): void {
+        this.contactsCombobox.selectedItems = [];
+        this.savedContacts = [];
+    }
+
+    public onContactChangesSubmit(): void { }
 }
