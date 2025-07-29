3c7ab0599344dd321db4d480a7a6df3962b3277e
Constantinos Leftheris <c.leftheris@indice.gr>
2025-07-29 18:24:13 +0300

    Update PushNotificationServiceAzure.cs

--- src/Indice.Services/PushNotificationServiceAzure.cs
+++ src/Indice.Services/PushNotificationServiceAzure.cs
@@ -100,7 +100,7 @@ public class PushNotificationServiceAzure : IPushNotificationService
         if (!string.IsNullOrEmpty(classification)) {
             properties.Add("classification", classification);
         }
-        NotificationOutcome outcome = null!;
+        NotificationOutcome outcome;
         if (tags?.Any() == true) {
             var tagsCollection = tags.Select(
                 tag => tag.Kind == PushNotificationTagKind.User || tag.Kind == PushNotificationTagKind.Unspecified
