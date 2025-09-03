591d26aa20b8212e7a7e2e24a0780ae0b4227e78
Vasileios Spyridonos <spyridonosvasileios@gmail.com>
2025-09-03 14:13:29 +0300

    replace type and template dropdowns in create campaign page with enhanced combobox component

--- src/Indice.Features.Messages.App/src/app/features/campaigns/create/steps/basic-info/campaign-basic-info.component.ts
+++ src/Indice.Features.Messages.App/src/app/features/campaigns/create/steps/basic-info/campaign-basic-info.component.ts
@@ -1,9 +1,9 @@
 import { DatePipe } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { AbstractControl, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 import { MenuOption } from '@indice/ng-components';
-import { map } from 'rxjs/operators';
+import { map, finalize } from 'rxjs/operators';
 import { MessagesApiClient, MessageTypeResultSet, TemplateListItemResultSet } from 'src/app/core/services/messages-api.service';
 
 @Component({
@@ -20,6 +20,9 @@ export class CampaignBasicInfoComponent implements OnInit {
   @Input() public hasTemplateLoaded: any = {};
   @Output() public templateSelected: EventEmitter<string | undefined> = new EventEmitter<string | undefined>();
   // Form Controls
+  @ViewChild('typeCombobox') typeCombobox: any;
+  @ViewChild('templateCombobox') templateCombobox: any;
+
   public get title(): AbstractControl { return this.form.get('title')!; }
   public get from(): AbstractControl { return this.form.get('from')!; }
   public get to(): AbstractControl { return this.form.get('to')!; }
@@ -32,9 +35,14 @@ export class CampaignBasicInfoComponent implements OnInit {
   // Properties
   public form!: FormGroup;
   public messageTypes: MenuOption[] = [new MenuOption('Παρακαλώ επιλέξτε...', null)];
+  public messageTypesForCombobox: any[] = [];
   public templates: MenuOption[] = [new MenuOption('Παρακαλώ επιλέξτε...', null)];
+  public templatesForCombobox: any[] = [];
   public now: Date = new Date();
   public messageType?: string;
+  public templatesLoading: boolean = false;
+
+  public equalityPredicate = (x: any, y: any) => x && y && x.value === y.value;
 
   public ngOnInit(): void {
     this._initForm();
@@ -49,76 +57,132 @@ export class CampaignBasicInfoComponent implements OnInit {
     this.to.setValue(this._datePipe.transform(event.target.value, 'yyyy-MM-ddTHH:mm'));
   }
 
+  public onMessageTypeSearch(searchTerm: string | undefined): void {
+    if (!this.messageTypesForCombobox.length) {
+      this._loadMessageTypes();
+    }
+  }
 
   public onMessageTypeSelectionChanged(event: any): void {
-    console.log('onMessageTypeSelectionChanged');
-    if (event.value) {
+    if (event && event.value) {
       this.messageType = event.value;
       this.type.setValue(event);
+
+      // Reset templates when message type changes
+      this.templatesForCombobox = [];
+      if (this.templateCombobox) {
+        this.templateCombobox.selectedItems = [];
+        this.templateCombobox.value = undefined;
+      }
+      this._loadTemplates();
     } else {
       this.messageType = undefined;
       this.type.setValue(null);
     }
-    console.log('reload');
-    this.templates = [new MenuOption('Παρακαλώ επιλέξτε...', null)];
-    this._loadTemplates();
+  }
+
+  public onTemplateSearch(searchTerm: string | undefined): void {
+    if (this.needsTemplate.value === 'yes' && this.templatesForCombobox.length === 0) {
+      this._loadTemplates();
+    }
+  }
+
+  public onTemplateSelectionChanged(event: any): void {
+    if (event && event.value) {
+      this.template.setValue(event);
+
+      const channelsFormArray: FormArray = this.channels as FormArray;
+      channelsFormArray.clear();
+
+      if (!this.actionLinkText.value) {
+        this.actionLinkText.setValue("Click me!");
+      }
+
+      if (!this.actionLinkHref.value) {
+        this.actionLinkHref.setValue("https://www.indice.gr");
+      }
+
+      if (event.data) {
+        event.data.forEach((channel: string) => channelsFormArray.push(new FormControl(channel)));
+      }
+
+      this.templateSelected.emit(event.value);
+    } else {
+      this.template.setValue(null);
+      this.templateSelected.emit(undefined);
+    }
   }
 
   public onNeedsTemplateChanged(event: any): void {
     const value = event.target.value;
+
     if (value === 'yes') {
-      if (this.templates.length <= 1) {
+      if (this.templatesForCombobox.length === 0) {
         this._loadTemplates();
       }
       this.template.setValidators(Validators.required);
     } else {
       this.template.removeValidators(Validators.required);
       this.template.setValue(null);
+
+      // Clear the combobox selection if it exists
+      if (this.templateCombobox) {
+        this.templateCombobox.selectedItems = [];
+        this.templateCombobox.value = undefined;
+      }
     }
+
     this.template.updateValueAndValidity();
     this.needsTemplate.setValue(value);
   }
-
-  public onTemplateSelectionChanged(event: any): void {
-    if (event.value) {
-      this.template.setValue(event);
-      const channelsFormArray: FormArray = this.channels as FormArray;
-      channelsFormArray.clear();
-      if (!this.actionLinkText.value) {
-        this.actionLinkText.setValue("Click me!");
-      }
-      if (!this.actionLinkHref.value) {
-        this.actionLinkHref.setValue("https://www.indice.gr");
-      }
-      event.data.forEach((channel: string) => channelsFormArray.push(new FormControl(channel)));
-    } else {
-      this.template.setValue(null);
-    }
-    this.templateSelected.emit(event.value);
-  }
-
-
 
   private _loadMessageTypes(): void {
     this._api
       .getMessageTypes()
       .pipe(map((messageTypes: MessageTypeResultSet) => {
         if (messageTypes.items) {
+          // Keep the original menu options for backward compatibility
+          this.messageTypes = [new MenuOption('Παρακαλώ επιλέξτε...', null)];
           this.messageTypes.push(...messageTypes.items.map(type => new MenuOption(type.name || '', type.id)));
+
+          // Transform for enhanced combobox
+          this.messageTypesForCombobox = messageTypes.items.map(type => ({
+            label: type.name || '',
+            value: type.id,
+            toString: function () { return this.label; } // Add toString method for proper string representation
+          }));
         }
       }))
       .subscribe();
   }
 
   private _loadTemplates(): void {
-    console.log(this.messageType);
+    this.templatesLoading = true;
+
     this._api
       .getTemplates(undefined, undefined, undefined, undefined, this.messageType, true)
-      .pipe(map((templates: TemplateListItemResultSet) => {
-        if (templates.items) {
-          this.templates.push(...templates.items.map(template => new MenuOption(template.name || '', template.id, undefined, template.channels)))
-        }
-      }))
+      .pipe(
+        map((templates: TemplateListItemResultSet) => {
+          if (templates.items) {
+            // Keep the original menu options for backward compatibility
+            this.templates = [new MenuOption('Παρακαλώ επιλέξτε...', null)];
+            this.templates.push(...templates.items.map(template =>
+              new MenuOption(template.name || '', template.id, undefined, template.channels)
+            ));
+
+            // Transform for enhanced combobox
+            this.templatesForCombobox = templates.items.map(template => ({
+              label: template.name || '',
+              value: template.id,
+              data: template.channels,
+              toString: function () { return this.label; } // Add toString method for proper string representation
+            }));
+          }
+        }),
+        finalize(() => {
+          this.templatesLoading = false;
+        })
+      )
       .subscribe();
   }
 
