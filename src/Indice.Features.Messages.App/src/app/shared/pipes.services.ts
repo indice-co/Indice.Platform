7eaa005d54da93060f8422a028bdbade243208a2
Constantinos Leftheris <c.leftheris@indice.gr>
2025-09-10 18:32:45 +0300

    [messaging] ui enhancements (#729)

--- src/Indice.Features.Messages.App/src/app/shared/pipes.services.ts
+++ src/Indice.Features.Messages.App/src/app/shared/pipes.services.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: 'beautifyBoolean' })
 export class BeautifyBooleanPipe implements PipeTransform {
     transform(value: boolean | undefined): string {
-        return value ? '<span class="ms-Icon ms-Icon--StatusCircleCheckmark text-green-500 text-3xl"></span>' : '<span class="ms-Icon ms-Icon--StatusCircleBlock2 text-red-500 text-3xl"></span>';
+      return value ? '<span class="ms-Icon ms-Icon--StatusCircleCheckmark list-checkmark"></span>' : '<span class="ms-Icon ms-Icon--StatusCircleBlock2 list-dash"></span>';
     }
 }
