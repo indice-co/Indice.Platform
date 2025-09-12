288ca347d12f0566d7590de26a06dc637661e681
travlos <travlos7@hotmail.com>
2025-09-12 17:20:58 +0300

    Cleanup unused using directive in Contact.cs
    Removed the unnecessary `using System.Runtime.Intrinsics.X86;` directive from Contact.cs. The namespace declaration was retained and slightly reformatted, indicating a code cleanup to improve readability and maintainability.

--- src/Indice.Features.Messages.Core/Models/Contact.cs
+++ src/Indice.Features.Messages.Core/Models/Contact.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.Intrinsics.X86;
-
-namespace Indice.Features.Messages.Core.Models;
+﻿namespace Indice.Features.Messages.Core.Models;
 
 /// <summary>Models a contact in the system as a member of a distribution list.</summary>
 public class Contact
