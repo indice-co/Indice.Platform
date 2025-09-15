c61ff22fb610f6b838264dcaf38310a8ff49dbfd
travlos <travlos7@hotmail.com>
2025-09-15 19:22:51 +0300

    Ommit MemberRole check if no user roles were given.

--- src/Indice.Features.Cases.Core/Data/Models/DbCaseAccessRule.cs
+++ src/Indice.Features.Cases.Core/Data/Models/DbCaseAccessRule.cs
@@ -26,9 +26,9 @@ public class DbCaseAccessRule
 
     public static Expression<Func<DbCaseAccessRule, bool>> AccessMatchPredicate(string? userId, List<string> userRoles, string? groupId) {
         return x =>
-            (userId != null && x.MemberUserId == userId) ||
-            (EF.Constant(userRoles).Contains(x.MemberRole!)) ||
-            (groupId != null && x.MemberGroupId == groupId);
+            userRoles.Count > 0 ?
+                (userId != null && x.MemberUserId == userId) || (groupId != null && x.MemberGroupId == groupId) || EF.Constant(userRoles).Contains(x.MemberRole!) :
+                (userId != null && x.MemberUserId == userId) || (groupId != null && x.MemberGroupId == groupId);
     }
     public static Expression<Func<DbCaseAccessRule, bool>> RuleMatchPredicate(Guid caseId, Guid caseTypeId, Guid checkpointTypeId) {
         return x =>
