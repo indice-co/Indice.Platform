5221ee294b1a0a62b4fe90afc73f9be9380a7b1f
travlos <travlos7@hotmail.com>
2025-09-15 19:03:29 +0300

    [cases] fix for sqlparameterexpception
    - Switch `AccessMatchPredicate` to use EF's constant handling for user roles.
    - Increment `VersionPrefixCases` in `Directory.Build.props`.

--- src/Indice.Features.Cases.Core/Data/Models/DbCaseAccessRule.cs
+++ src/Indice.Features.Cases.Core/Data/Models/DbCaseAccessRule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Indice.Features.Cases.Core.Data.Models;
 
@@ -26,7 +27,7 @@ public class DbCaseAccessRule
     public static Expression<Func<DbCaseAccessRule, bool>> AccessMatchPredicate(string? userId, List<string> userRoles, string? groupId) {
         return x =>
             (userId != null && x.MemberUserId == userId) ||
-            (userRoles.Any() && userRoles.Contains(x.MemberRole!)) ||
+            (EF.Constant(userRoles).Contains(x.MemberRole!)) ||
             (groupId != null && x.MemberGroupId == groupId);
     }
     public static Expression<Func<DbCaseAccessRule, bool>> RuleMatchPredicate(Guid caseId, Guid caseTypeId, Guid checkpointTypeId) {
