1c2ec1ae206ad204b6bbed276ee4a472addd571f
Constantinos Leftheris <c.leftheris@live.com>
2025-09-22 17:10:27 +0300

    minor code ql suggestion

--- src/Indice.Features.Messages.Core/Services/MessageEventService.cs
+++ src/Indice.Features.Messages.Core/Services/MessageEventService.cs
@@ -3,6 +3,7 @@ using Indice.Features.Messages.Core.Events;
 using Indice.Features.Messages.Core.Models.Requests;
 using Indice.Features.Messages.Core.Services.Abstractions;
 using Indice.Types;
+using Org.BouncyCastle.Security.Certificates;
 
 namespace Indice.Features.Messages.Core.Services;
 
@@ -28,21 +29,23 @@ public class MessageEventService : IMessageEventService
             CreatedOn = x.CreatedOn,
             MessageId = x.MessageId
         });
-        if (options.Filter.CampaignId.HasValue) {
-            query= query.Where(x => x.CampaignId == options.Filter.CampaignId.Value);
-        }
-        if (options.Filter.MessageId.HasValue) {
-            query = query.Where(x => x.MessageId == options.Filter.MessageId.Value);
-        }
-        if (options.Filter.RangeStart.HasValue) {
-            query = query.Where(x => x.CreatedOn >= options.Filter.RangeStart.Value);
-        }
-        if (options.Filter.RangeEnd.HasValue) {
-            query = query.Where(x => x.CreatedOn < options.Filter.RangeEnd.Value);
-        }
-        if (options.Filter.Channel?.Length > 0) {
-            var channels = options.Filter.Channel.Select(x => x.ToString());
-            query = query.Where(x => channels.Contains(x.Channel));
+        if (options.Filter is not null) { 
+            if (options.Filter.CampaignId.HasValue) {
+                query= query.Where(x => x.CampaignId == options.Filter.CampaignId.Value);
+            }
+            if (options.Filter.MessageId.HasValue) {
+                query = query.Where(x => x.MessageId == options.Filter.MessageId.Value);
+            }
+            if (options.Filter.RangeStart.HasValue) {
+                query = query.Where(x => x.CreatedOn >= options.Filter.RangeStart.Value);
+            }
+            if (options.Filter.RangeEnd.HasValue) {
+                query = query.Where(x => x.CreatedOn < options.Filter.RangeEnd.Value);
+            }
+            if (options.Filter.Channel?.Length > 0) {
+                var channels = options.Filter.Channel.Select(x => x.ToString());
+                query = query.Where(x => channels.Contains(x.Channel));
+            }
         }
         var term = options.Search?.Trim().ToLower();
         if (!string.IsNullOrWhiteSpace(term)) {
