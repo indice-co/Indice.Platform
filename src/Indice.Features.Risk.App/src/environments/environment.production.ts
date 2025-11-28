ed677b1040fdb8c83c0d917eb3c308bbd0a8d68b
Constantinos Leftheris <c.leftheris@live.com>
2025-11-28 16:06:20 +0200

    Refactor and enhance SPA project structure and localization
    Refactored `SpaIndexFileInfo` to simplify return logic. Updated
    `Indice.Features.Risk.UI.csproj` to rename `SpaProjectName` to
    `browser`, adjust `SpaDistFolder`, and refine `EmbeddedResource`
    includes. Modified `RisksUIMiddlewareExtensions` to reflect the
    new `SpaProjectName`.
    
    Enhanced `RisksUIOptions` with a `Lang` property for localization
    and updated `ConfigureIndexParameters` to include language
    support. Refactored `index.html` with modern CSS layers,
    `modulepreload` support, and improved structure.
    
    Adjusted `.gitignore` to ensure `index.html` and `.gitignore` are
    not ignored. These changes improve maintainability, localization,
    and project structure alignment.

---
+++ src/Indice.Features.Risk.App/src/environments/environment.production.ts
@@ -0,0 +1,24 @@
+const title = document.title;
+
+export const environment = {
+  api_url: 'https://localhost:2001',
+  auth_settings: {
+    accessTokenExpiringNotificationTime: 60,
+    authority: 'https://my.indice.gr',
+    automaticSilentRenew: true,
+    client_id: 'risk-ui',
+    filterProtocolClaims: true,
+    loadUserInfo: true,
+    monitorSession: true,
+    post_logout_redirect_uri: 'http://localhost:4200',
+    redirect_uri: 'http://localhost:4200/auth-callback',
+    response_type: 'code',
+    revokeAccessTokenOnSignout: true,
+    scope: 'openid profile role email risk',
+    silent_redirect_uri: 'http://localhost:4200/auth-renew',
+    useRefreshToken: true
+  },
+  culture: 'el-GR',
+  isTemplate: false,
+  production: false
+};
