177b8933ae77180241fb7e1e9b32c87332a1981a
travlos <travlos7@hotmail.com>
2026-02-17 16:18:05 +0200

    Simplify terms acceptance requirement logic
    Refactored the conditional check for requiring acceptance of terms by removing a redundant hasAcceptedTerms check. The logic is now clearer and more concise, ensuring users are prompted to accept updated terms only when necessary.

--- src/Indice.Features.Identity.Core/IdentityValidation/IIdentityValidationActivity.cs
+++ src/Indice.Features.Identity.Core/IdentityValidation/IIdentityValidationActivity.cs
@@ -169,7 +169,7 @@ public class RequiresTermsAcceptanceActivity : IdentityValidationActivityBase
 
         if (requirePostSignInAcceptedTerms &&
             (!hasAcceptedTerms ||
-            (hasAcceptedTerms && latestTermsReleaseDate > DateTime.MinValue && acceptedTermsDate < latestTermsReleaseDate))) {
+            (latestTermsReleaseDate > DateTime.MinValue && acceptedTermsDate < latestTermsReleaseDate))) {
             return new UserValidationRequirement(UserActivityRequirementKind.RequiresAcceptanceOfTerms, "/AcceptTerms");
         }
         return null;
