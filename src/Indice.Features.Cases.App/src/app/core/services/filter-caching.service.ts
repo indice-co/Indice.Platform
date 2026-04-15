867e1c75fbc3b49b7fb183e2ce53dcf73e590843
Constantinos Leftheris <c.leftheris@live.com>
2026-04-15 14:11:21 +0300

    [codeql] Prefix filter cache keys with '$' in FilterCachingService
    All filter parameter keys are now stored with a '$' prefix to avoid object injection as well as potential key collisions. Updated setParams, getParams, and resetParams methods to use this prefix consistently.

--- src/Indice.Features.Cases.App/src/app/core/services/filter-caching.service.ts
+++ src/Indice.Features.Cases.App/src/app/core/services/filter-caching.service.ts
@@ -9,27 +9,27 @@ type Dictionary = {
 })
 export class FilterCachingService {
   private filterParams: Dictionary = {};
-
+  private readonly keyPrefix = '$';
   constructor() { }
 
   /**
    * Stores params.
    */
   setParams(key: string, filterParams: object) {
-    this.filterParams[key] = filterParams;
+      this.filterParams[this.keyPrefix + key] = filterParams;
   }
 
   /**
    * Gets Stored params.
    */
   getParams(key: string): object {
-    return this.filterParams[key];
+    return this.filterParams[this.keyPrefix + key];
   }
 
   /**
    * Clears Stored params.
    */
   resetParams(key: string) {
-    delete this.filterParams[key];
+    delete this.filterParams[this.keyPrefix + key];
   }
 }
