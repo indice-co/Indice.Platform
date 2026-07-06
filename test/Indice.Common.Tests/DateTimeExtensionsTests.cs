fdf8290cfbafaa794dad5890d696d1f6a6b19cc8
Constantinos Leftheris <c.leftheris@live.com>
2026-07-06 14:44:33 +0300

    [core] Add DateTimeOffset.Convert extension and related tests
    Updated XML docs to note support for Windows and IANA time zone IDs. Added a Convert extension method for DateTimeOffset to shift to a specified time zone. Introduced unit tests to verify correct time zone conversion.

---
+++ test/Indice.Common.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Indice.Extensions;
+using Xunit;
+
+namespace Indice.Common.Tests;
+
+public class DateTimeExtensionsTests
+{
+
+
+    [Trait("Tag", "TimeZone")]
+    [Fact]
+    public void ConvertTimeZoneFormUtc() {
+        var testDateTimeOffset = DateTimeOffset.Parse("2026-07-06T11:31:22.7300379+00:00");
+        var dateTimeToTargetOffset = testDateTimeOffset.Convert("Europe/Athens");
+        Assert.Equal("2026-07-06T14:31:22.7300379+03:00", dateTimeToTargetOffset.ToString("o"));
+    }
+}
