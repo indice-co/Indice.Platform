c0ff971fd34682848ed19183e5f9bcdb7d82d384
Constantinos Leftheris <c.leftheris@live.com>
2026-07-06 17:35:28 +0300

    Refactor ConvertTimeZoneFromUtc to use [Theory] test
    ConvertTimeZoneFromUtc test now uses [Theory] with multiple [InlineData] cases, enabling validation of DateTimeOffset conversion for various time zones and expected results. This improves test coverage and flexibility.

--- test/Indice.Common.Tests/DateTimeExtensionsTests.cs
+++ test/Indice.Common.Tests/DateTimeExtensionsTests.cs
@@ -8,10 +8,13 @@ public class DateTimeExtensionsTests
 
 
     [Trait("Tag", "TimeZone")]
-    [Fact]
-    public void ConvertTimeZoneFromUtc() {
+    [Theory]
+    [InlineData("Europe/Athens", "2026-07-06T14:31:22.7300379+03:00")]
+    [InlineData("America/New_York", "2026-07-06T07:31:22.7300379-04:00")]
+    [InlineData("GTB Standard Time", "2026-07-06T14:31:22.7300379+03:00")]
+    public void ConvertTimeZoneFromUtc(string timeZoneId, string expected) {
         var testDateTimeOffset = DateTimeOffset.Parse("2026-07-06T11:31:22.7300379+00:00");
-        var dateTimeToTargetOffset = testDateTimeOffset.Convert("Europe/Athens");
-        Assert.Equal("2026-07-06T14:31:22.7300379+03:00", dateTimeToTargetOffset.ToString("o"));
+        var dateTimeToTargetOffset = testDateTimeOffset.Convert(timeZoneId);
+        Assert.Equal(expected, dateTimeToTargetOffset.ToString("o"));
     }
 }
