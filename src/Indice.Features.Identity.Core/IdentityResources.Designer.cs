6e7f01863911cda9add263f396398ae284c314dd
travlos <travlos7@hotmail.com>
2026-09-07 16:02:33 +0300

    Update password confirmation validation resources
    Updated English summary for LimitAttemptsReached. Added new resource string properties for password confirmation errors and field names in IdentityResources.Designer.cs. Localized new password confirmation error messages in all supported .resx files.

--- src/Indice.Features.Identity.Core/IdentityResources.Designer.cs
+++ src/Indice.Features.Identity.Core/IdentityResources.Designer.cs
@@ -394,7 +394,7 @@ namespace Indice.Features.Identity.Core {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to You have reached the daily notification request limit. Please try again later..
+        ///   Looks up a localized string similar to You have reached the limit of attempts. Please try again later..
         /// </summary>
         internal static string LimitAttemptsReached {
             get {
@@ -869,7 +869,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_ChangePassword_NewPassword_FieldName", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The field &apos;Confirm Password&apos; is required..
         /// </summary>
@@ -878,7 +878,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_ChangePassword_NewPasswordConfirmation_Empty_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The password confirmation does not match the password..
         /// </summary>
@@ -887,7 +887,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_ChangePassword_NewPasswordConfirmation_Mismatch_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Old Password.
         /// </summary>
@@ -941,7 +941,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_ForgotPasswordConfirmation_NewPassword_FieldName", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The field &apos;Confirm Password&apos; is required..
         /// </summary>
@@ -950,7 +950,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_ForgotPasswordConfirmation_NewPasswordConfirmation_Empty_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The password confirmation does not match the password..
         /// </summary>
@@ -959,7 +959,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_ForgotPasswordConfirmation_NewPasswordConfirmation_Mismatch_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Token.
         /// </summary>
@@ -1166,7 +1166,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_Register_Password_FieldName", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The field &apos;Confirm Password&apos; is required..
         /// </summary>
@@ -1175,7 +1175,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_Register_PasswordConfirmation_Empty_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The password confirmation does not match the password..
         /// </summary>
@@ -1184,7 +1184,7 @@ namespace Indice.Features.Identity.Core {
                 return ResourceManager.GetString("UI_Validator_Register_PasswordConfirmation_Mismatch_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to The field &apos;Phone Number&apos; has invalid format..
         /// </summary>
