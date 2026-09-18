ea38c7f93e700ac663c970b47a69f55bdc010c11
Constantinos Leftheris <c.leftheris@live.com>
2026-09-18 15:53:49 +0300

    Remove unused System.Xml.Linq using directive
    The using directive for System.Xml.Linq was removed from ChatMessagePart.cs as it was not being used. No other changes were made.

--- src/Indice.Features.Agents.Core/Models/ChatMessagePart.cs
+++ src/Indice.Features.Agents.Core/Models/ChatMessagePart.cs
@@ -3,7 +3,6 @@ using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Xml.Linq;
 using Microsoft.Extensions.AI;
 
 namespace Indice.Features.Agents.Core.Models;
